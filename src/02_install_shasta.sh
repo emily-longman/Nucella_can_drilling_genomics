2de1ce7f36972860b9697f5d0309226ceddc3522
Emily Longman <158350049+emily-longman@users.noreply.github.com>
2024-03-06 13:55:25 -0500

    Script to Install Shasta

---
+++ src/02_install_shasta.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Install Shasta (https://paoloshasta.github.io/shasta/QuickStart.html)
+
+# Download the executable for the latest release.
+curl -O -L https://github.com/chanzuckerberg/shasta/releases/download/0.11.1/shasta-Linux-0.11.1
+
+# Grant execute permissions.
+chmod ugo+x shasta-Linux-0.11.1
