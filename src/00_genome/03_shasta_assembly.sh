8b384b9ccd5a82e9bf44cf6d7569361af8030736
Emily Longman <158350049+emily-longman@users.noreply.github.com>
2024-03-09 12:27:31 -0500

    Update 03_shasta_assembly.sh

--- src/00_genome/03_shasta_assembly.sh
+++ src/00_genome/03_shasta_assembly.sh
@@ -30,14 +30,14 @@
 cd /netfiles/pespenilab_share/Nucella/processed/Base_Genome/
 
 #executable
-#shasta=/gpfs1/home/e/l/elongman/software/shasta/shasta-Linux-0.11.1 #this doesn't work - likely mine is not initialized?
-shasta=/netfiles/nunezlab/Shared_Resources/Software/shasta/shasta-Linux-0.10.0
+shasta=/gpfs1/home/e/l/elongman/software/shasta/shasta-Linux-0.11.1 
+#shasta=/netfiles/nunezlab/Shared_Resources/Software/shasta/shasta-Linux-0.10.0
 
-# If you haven't done it yet, unzip the files 
-#gunzip /gpfs2/scratch/elongman/Nucella_can_drilling_genomics/data/processed/ONT_fltlong/*fastq.gz
+# If you haven't done it yet, gunzip the files 
+#gunzip /netfiles/pespenilab_share/Nucella/processed/ONT_fltlong/*fastq.gz
 
 #input
-infa=/gpfs2/scratch/elongman/Nucella_can_drilling_genomics/data/processed/ONT_fltlong/Nuc.3500.fltlong.fastq
+infa=/netfiles/pespenilab_share/Nucella/processed/ONT_fltlong/Nuc.3500.fltlong.fastq
 
 #run shasta
 $shasta --input $infa --config Nanopore-May2022
