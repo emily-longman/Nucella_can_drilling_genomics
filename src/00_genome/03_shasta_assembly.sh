0f6e799619f020d6997ebfa3fa0012459aea518c
Emily Longman <158350049+emily-longman@users.noreply.github.com>
2024-03-21 17:20:00 -0400

    Update 03_shasta_assembly.sh

--- src/00_genome/03_shasta_assembly.sh
+++ src/00_genome/03_shasta_assembly.sh
@@ -11,14 +11,17 @@
 #SBATCH --partition=bluemoon
 
 # Request nodes
-#SBATCH --cpus-per-task=40 
-#SBATCH --nodes=4 # on four nodes
+#SBATCH --cpus-per-task=1 
+#SBATCH --nodes=1 # on four nodes
 
 # Reserve walltime -- hh:mm:ss --30 hrs max
-#SBATCH --time=30:00:00 #<= this may depend on your resources
+#SBATCH --time=90:00:00 #<= this may depend on your resources
 
 # Request memory for the entire job -- you can request --mem OR --mem-per-cpu
-#SBATCH --mem=200G #<= this may depend on your resources
+#SBATCH --mem=600G #<= this may depend on your resources
+
+# Submit job array
+#SBATCH --array=0-2
 
 # Name output of this job using %x=job-name and %j=job-id
 #SBATCH --output=./slurmOutput/shasta.%x_%j.out # Standard output
@@ -33,11 +36,16 @@ cd /gpfs2/scratch/elongman/Nucella_can_drilling_genomics/data/processed/
 #executable
 shasta=/netfiles/nunezlab/Shared_Resources/Software/shasta/shasta-Linux-0.10.0
 
+arr=(2000 3500 5000)
+L="${arr[$SLURM_ARRAY_TASK_ID]}"
+echo $L
+
 # If you haven't done it yet, gunzip the files 
 # gunzip /gpfs2/scratch/elongman/Nucella_can_drilling_genomics/data/processed/ONT_fltlong/*fastq.gz
 
 #input
-infa=/gpfs2/scratch/elongman/Nucella_can_drilling_genomics/data/processed/ONT_fltlong/Nuc.3500.fltlong.fastq
+infa=/gpfs2/scratch/elongman/Nucella_can_drilling_genomics/data/processed/ONT_fltlong
 
 #run shasta
-$shasta --input $infa --config Nanopore-May2022
+$shasta --input $infa/Nuc.$L.fltlong.fastq.gz --config Nanopore-May2022
+
