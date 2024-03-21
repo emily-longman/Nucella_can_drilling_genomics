8a0ed5a5bcb46563dfab1276a889ce91ffdde49b
Emily Longman <158350049+emily-longman@users.noreply.github.com>
2024-03-21 14:24:10 -0400

    Update 03_map_reads.sh

--- src/02_map_reads/03_map_reads.sh
+++ src/02_map_reads/03_map_reads.sh
@@ -39,12 +39,35 @@ module load bwa-0.7.17-gcc-7.3.0-terdbma
 READS=/gpfs2/scratch/elongman/Nucella_can_drilling_genomics/data/processed/clean_Shortreads
 
 #This is the location where the reference genome and all its indexes are stored.
-REFERENCE=/netfiles/pespenilab_share/Nucella/processed/Base_Genome/ShastaRun
-
+REFERENCE=/netfiles/pespenilab_share/Nucella/processed/Base_Genome/ShastaRun/Assembly.fasta
 
 #--------------------------------------------------------------------------------
 
 
 # This indexing step only needs to be done once for the reference file.
-bwa index -p ref -a is $REFERENCE/Assembly.fasta
+bwa index -p ref -a bwtsw $REFERENCE 
+# -p is the preix of the output databse 
+# -a is the algorithm for constructing BWT index ('is' - linear-time algorithm for constructing suffix array; bwtsw 
 
+#--------------------------------------------------------------------------------
+
+
+MyLeftReads=`find $READS -maxdepth 1 -name "*_R1*.fastq.gz"`
+
+#Making absolutely sure that reads are in the correct order:
+for myLeft in $MyLeftReads
+do
+    #The mate file will have a _R2 instead of a _R1. This changes the first occurence of _R1 with _R2.
+    myRight=${myLeft/_R1/_R2}
+    #Is there a mate file?
+    if [ -f $myRight ]
+    then
+        myName=`echo $myLeft | cut -c3-16` # shortened name
+         bwa mem \
+         -t 5 \
+         $REFERENCE \
+         $READS/$myLeft \
+         $READS/$myRight \
+         > $READS/$myName.sam 
+    fi
+done
