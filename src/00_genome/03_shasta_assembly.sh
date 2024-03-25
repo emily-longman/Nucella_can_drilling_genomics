79a3e3d0d1240df1513c38a6517996c4a6f4695f
Emily Longman <158350049+emily-longman@users.noreply.github.com>
2024-03-25 12:51:34 -0400

    Update 03_shasta_assembly.sh

--- src/00_genome/03_shasta_assembly.sh
+++ src/00_genome/03_shasta_assembly.sh
@@ -21,7 +21,7 @@
 #SBATCH --mem=600G #<= this may depend on your resources
 
 # Submit job array
-#SBATCH --array=0-2
+#SBATCH --array=0-4
 
 # Name output of this job using %x=job-name and %j=job-id
 #SBATCH --output=./slurmOutput/shasta.%x_%j.out # Standard output
@@ -30,22 +30,25 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=emily.longman@uvm.edu # where to email updates to
 
+#--------------------------------------------------------------------------------
+
 # Move to the directory where the output files will be saved
-cd /gpfs2/scratch/elongman/Nucella_can_drilling_genomics/data/processed/
+cd /gpfs2/scratch/elongman/Nucella_can_drilling_genomics/data/processed/Shasta_assemblies/
 
 #executable
 shasta=/netfiles/nunezlab/Shared_Resources/Software/shasta/shasta-Linux-0.10.0
 
-arr=(3500 5000 10000)
+arr=(1000 2000 3500 5000 10000)
 L="${arr[$SLURM_ARRAY_TASK_ID]}"
 echo $L
 
 # If you haven't done it yet, gunzip the files 
-gunzip /gpfs2/scratch/elongman/Nucella_can_drilling_genomics/data/processed/ONT_fltlong/*fastq.gz
+#gunzip /gpfs2/scratch/elongman/Nucella_can_drilling_genomics/data/processed/ONT_fltlong/*fastq.gz
 
 #input
 infa=/gpfs2/scratch/elongman/Nucella_can_drilling_genomics/data/processed/ONT_fltlong
+conf=/gpfs2/scratch/elongman/Nucella_can_drilling_genomics/src/00_genome/shasta_config
 
 #run shasta
-$shasta --input $infa/Nuc.$L.fltlong.fastq --config Nanopore-May2022 --assemblyDirectory ShastaRun$L
+$shasta --input $infa/Nuc.$L.fltlong.fastq --config $conf/Nanopore-May2022_$L.sh --assemblyDirectory ShastaRun$L
 
