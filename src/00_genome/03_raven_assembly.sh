fcc8da337a6576064de8bac90b459b15b47b77cd
Emily Longman <158350049+emily-longman@users.noreply.github.com>
2024-03-27 13:37:12 -0400

    Update 03_raven_assembly.sh

--- src/00_genome/03_raven_assembly.sh
+++ src/00_genome/03_raven_assembly.sh
@@ -20,9 +20,6 @@
 # Request memory for the entire job -- you can request --mem OR --mem-per-cpu
 #SBATCH --mem=900G #<= this may depend on your resources
 
-# Submit job array
-#SBATCH --array=0-4
-
 # Name output of this job using %x=job-name and %j=job-id
 #SBATCH --output=./slurmOutput/%x_%j.out # Standard output
 
