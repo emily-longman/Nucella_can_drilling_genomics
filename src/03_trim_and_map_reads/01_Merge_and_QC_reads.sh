a187e44a47cefb5ca74ca67544fc3cc31886179f
Emily Longman <158350049+emily-longman@users.noreply.github.com>
2024-04-25 17:04:14 -0400

    Update 01_Merge_and_QC_reads.sh

--- src/03_trim_and_map_reads/01_Merge_and_QC_reads.sh
+++ src/03_trim_and_map_reads/01_Merge_and_QC_reads.sh
@@ -21,7 +21,7 @@
 #SBATCH --mem=20G 
 
 # Submit job array
-#SBATCH --array=1
+#SBATCH --array=1-5
 
 # Name output of this job using %x=job-name and %j=job-id
 #SBATCH --output=./slurmOutput/%x_%j.out # Standard output
