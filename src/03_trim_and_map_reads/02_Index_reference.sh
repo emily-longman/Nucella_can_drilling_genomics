8069be4f6dcc78defd6d9594e8d0066227d61f2d
Emily Longman <158350049+emily-longman@users.noreply.github.com>
2024-05-08 05:58:14 -0400

    Update 02_Index_reference.sh

--- src/03_trim_and_map_reads/02_Index_reference.sh
+++ src/03_trim_and_map_reads/02_Index_reference.sh
@@ -18,7 +18,7 @@
 #SBATCH --time=24:00:00 
 
 # Request memory for the entire job -- you can request --mem OR --mem-per-cpu
-#SBATCH --mem=100G 
+#SBATCH --mem=80G 
 
 # Name output of this job using %x=job-name and %j=job-id
 #SBATCH --output=./slurmOutput/%x_%j.out # Standard output
