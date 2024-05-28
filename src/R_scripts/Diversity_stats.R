734e5489e1b1018050c73440ddcefa734b4e1144
Emily Longman <Emily.Longman@uvm.edu>
2024-05-28 15:41:37 -0400

    Re-arrange script numbers

--- src/R_scripts/Diversity_stats.R
+++ src/R_scripts/Diversity_stats.R
@@ -16,7 +16,6 @@ theta.FB <- read.table("FB_SNPs.thetas", sep="\t", header=T)
 theta.HC <- read.table("FB_SNPs.thetas", sep="\t", header=T)
 theta.MP <- read.table("FB_SNPs.thetas", sep="\t", header=T)
 
-
 theta.FB$tWsite = theta.FB$tW/theta.FB$nSites #scales the theta-W by the number of sites
 theta.FB$tPsite = theta.FB$tP/theta.FB$nSites #scales the theta-Pi by the number of sites
 
@@ -25,7 +24,6 @@ summary(theta.FB)
 # You can order the contig list to show you the contigs with the highest values of Tajima's D, or the lowest
 
 head(theta.FB[order(theta.FB$Tajima, decreasing = TRUE),]) # top 10 Tajima's D values
-
 head(theta.FB[order(theta.FB$Tajima, decreasing = FALSE),]) # bottom 10 Tajima's D values
 
 #You can also look for contigs that have combinations of high Tajima's D and low diversity -- these could represent outliers for selection
@@ -35,7 +33,6 @@ sfs.FB<-scan('FB_SNPs.sfs')
 sfs.FB<-sfs.FB[-c(1,which(sfs.FB==0))]
 sfs.FB<-sfs.FB/sum(sfs.FB)
 
-# Be sure to replace "9999" with your pop code in the "main" legend below
 barplot(sfs.FB, xlab="Contigs",
         names=1:length(sfs.FB),
         ylab="Proportions",
@@ -47,7 +44,8 @@ par(mfrow=c(2,2))
 hist(theta.FB$tWsite, xlab="theta-W", main="Watterson's theta")
 hist(theta.FB$tPsite, xlab="theta-Pi", main="Pairwise Nucleotide Diversity")
 hist(theta.FB$Tajima, xlab="D", main="Tajima's D")
-barplot(sfs.FB,names=1:length(sfs.FB),main='Site Frequency Spectrum')
+barplot(sfs.FB, names=1:length(sfs.FB), main='Site Frequency Spectrum') 
+
 dev.off() # To reset the panel plotting
 
 
