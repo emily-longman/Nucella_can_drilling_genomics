a6ba154478f8598af4d228c0a61ed5d97c8daec8
Emily Longman <Emily.Longman@uvm.edu>
2024-06-04 18:46:46 -0400

    Update 06_Coverage.sh

--- src/03_Trim_and_Map_Reads/06_Coverage.sh
+++ src/03_Trim_and_Map_Reads/06_Coverage.sh
@@ -5,20 +5,20 @@
 # Request cluster resources ----------------------------------------------------
 
 # Name this job
-#SBATCH --job-name=multiqc_qualimap
+#SBATCH --job-name=qualimap_multi_bamqc
 
 # Specify partition
-#SBATCH --partition=bluemoon
+#SBATCH --partition=bigmem
 
 # Request nodes
 #SBATCH --nodes=1 
 #SBATCH --ntasks-per-node=1
 
 # Reserve walltime -- hh:mm:ss --30 hrs max
-#SBATCH --time=8:00:00 
+#SBATCH --time=24:00:00 
 
 # Request memory for the entire job -- you can request --mem OR --mem-per-cpu
-#SBATCH --mem=50G 
+#SBATCH --mem=500G 
 
 # Name output of this job using %x=job-name and %j=job-id
 #SBATCH --output=./slurmOutput/%x_%j.out # Standard output
@@ -37,11 +37,32 @@ qualimap=/netfiles/nunezlab/Shared_Resources/Software/qualimap_v2.2.1/qualimap
 WORKING_FOLDER=/gpfs2/scratch/elongman/Nucella_can_drilling_genomics/data/processed/fastq_to_VCF
 
 #--------------------------------------------------------------------------------
+# Define parameters
+JAVAMEM=18G # Java memory
+
+#--------------------------------------------------------------------------------
+
+# Move to working folder
+cd $WORKING_FOLDER
+
+# Generate output folder 
+if [ -d "qualimap_multi_bamqc" ]
+then
+echo "Working qualimap_multi_bamqc folder exist"
+echo "lets move on"
+else 
+echo "Folder doesnt exist. Lets fix that"
+mkdir qualimap_multi_bamqc
+fi
+
+OUTPUT=$WORKING_FOLDER/qualimap_multi_bamqc
+
+#--------------------------------------------------------------------------------
 
 ## Read guide files
 # This is a file with the name all the samples to be processed. One sample name per line with all the info.
 
-GUIDE_FILE=/gpfs2/scratch/elongman/Nucella_can_drilling_genomics/data/processed/fastq_to_VCF/multiqc_qualimap/Qualimap_bam_list.txt
+GUIDE_FILE=/gpfs2/scratch/elongman/Nucella_can_drilling_genomics/data/processed/fastq_to_VCF/Guide_Files/Qualimap_bam_list.txt
 
 # Use script below to make list of the path to the folder which contains results of BAM QC analysis. 
 # Download and open file in excel. Add column with name of the sample
@@ -49,25 +70,11 @@ GUIDE_FILE=/gpfs2/scratch/elongman/Nucella_can_drilling_genomics/data/processed/
 #cd $WORKING_FOLDER/Merged_Bams_qualimap
 # Create a list for each collection location
 #ls -d "$PWD/"Qualimap_LaneMerged_* > $OUTPUT/Qualimap_bam_list.txt 
-#--------------------------------------------------------------------------------
-# Move to working folder
-cd $WORKING_FOLDER
-
-# Generate output folder 
-if [ -d "qualimap_multi-bamqc" ]
-then
-echo "Working qualimap_multi folder exist"
-echo "lets move on"
-else 
-echo "Folder doesnt exist. Lets fix that"
-mkdir qualimap_multi
-fi
-
-OUTPUT=$WORKING_FOLDER/qualimap_multi
 
 #--------------------------------------------------------------------------------
 
 # Assess quality of all bam files
 $qualimap multi-bamqc \
 -d $GUIDE_FILE \
--outdir $OUTPUT
+-outdir $OUTPUT \
+--java-mem-size=$JAVAMEM
