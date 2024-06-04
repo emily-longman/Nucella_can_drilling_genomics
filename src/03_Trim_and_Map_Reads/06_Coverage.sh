87a28c0c767a74380ec64f1d62ae375a8f4f1653
Emily Longman <Emily.Longman@uvm.edu>
2024-06-04 17:06:26 -0400

    Scripts to calculate average coverage

--- src/03_Trim_and_Map_Reads/06_Coverage.sh
+++ src/03_Trim_and_Map_Reads/06_Coverage.sh
@@ -18,10 +18,7 @@
 #SBATCH --time=8:00:00 
 
 # Request memory for the entire job -- you can request --mem OR --mem-per-cpu
-#SBATCH --mem=20G 
-
-# Submit job array
-#SBATCH --array=1-576%20
+#SBATCH --mem=50G 
 
 # Name output of this job using %x=job-name and %j=job-id
 #SBATCH --output=./slurmOutput/%x_%j.out # Standard output
@@ -31,67 +28,46 @@
 #SBATCH --mail-user=emily.longman@uvm.edu 
 
 #--------------------------------------------------------------------------------
-
 # This script will calculate average coverage 
 
-#--------------------------------------------------------------------------------
-# Call package (installed with conda)
-module load python3.11-anaconda/2023.09-0
-source ${ANACONDA_ROOT}/etc/profile.d/conda.sh
-conda create --name multiqc #create and name the environment
-source activate multiqc #activate the environment
-conda install -c bioconda multiqc # install the program
-conda activate multiqc 
-#--------------------------------------------------------------------------------
-
-## PREPARE GUIDE FILES
-## Read guide files
-# This is a file with the name all the samples to be processed. One sample name per line with all the info.
-
-SAMPLE_FILE=/gpfs2/scratch/elongman/Nucella_can_drilling_genomics/data/processed/fastq_to_VCF/GuideFileMerged.txt
-
-#Example: -- the headers are just for descriptive purposes. The actual file has no headers.
-## Snail_ID  Sample#   Merged_name 1    Merged_name 2    Merged_name 3   Merged_bam_name
-##  FB1-1     S84     FB1-1_S84_L002   FB1-1_S84_L007   FB1-1_S84_L008     FB1-1_S84
-##  FB1-2     S173    FB1-2_S173_L002  FB1-2_S173_L007  FB1-2_S173_L008    FB1-2_S173
-##  FB1-5     S109    FB1-5_S109_L002  FB1-5_S109_L007  FB1-5_S109_L008    FB1-5_S109
-##  ...
-##  MP9-9     S191    MP9-9_S191_L002  MP9-9_S191_L007  MP9-9_S191_L008    MP9-9_S191
-##  MP9-10    S26     MP9-10_S26_L002  MP9-10_S26_L007  MP9-10_S26_L008    MP9-10_S26
-
-#--------------------------------------------------------------------------------
-
-# Determine sample to process, "i" and read files
-i=`awk -F "\t" '{print $6}' $SAMPLE_FILE | sed "${SLURM_ARRAY_TASK_ID}q;d"`
-echo $i
-
-#--------------------------------------------------------------------------------
+#Load modules 
+qualimap=/netfiles/nunezlab/Shared_Resources/Software/qualimap_v2.2.1/qualimap
 
 #Working folder is core folder where this pipeline is being run.
 WORKING_FOLDER=/gpfs2/scratch/elongman/Nucella_can_drilling_genomics/data/processed/fastq_to_VCF
 
+#--------------------------------------------------------------------------------
+
+## Read guide files
+# This is a file with the name all the samples to be processed. One sample name per line with all the info.
+
+GUIDE_FILE=/gpfs2/scratch/elongman/Nucella_can_drilling_genomics/data/processed/fastq_to_VCF/multiqc_qualimap/Qualimap_bam_list.txt
+
+# Use script below to make list of the path to the folder which contains results of BAM QC analysis. 
+# Download and open file in excel. Add column with name of the sample
+# Move to qualimap folder
+#cd $WORKING_FOLDER/Merged_Bams_qualimap
+# Create a list for each collection location
+#ls -d "$PWD/"Qualimap_LaneMerged_* > $OUTPUT/Qualimap_bam_list.txt 
+#--------------------------------------------------------------------------------
+# Move to working folder
 cd $WORKING_FOLDER
 
-if [ -d "multiqc_qualimap" ]
+# Generate output folder 
+if [ -d "qualimap_multi-bamqc" ]
 then
-echo "Working multiqc_qualimap folder exist"
+echo "Working qualimap_multi folder exist"
 echo "lets move on"
 else 
 echo "Folder doesnt exist. Lets fix that"
-mkdir multiqc_qualimap
+mkdir qualimap_multi
 fi
 
-#--------------------------------------------------------------------------------
-
-# Put a copy of each qualimap file in the multiqc_qualimap folder
-scp $WORKING_FOLDER/Merged_Bams_qualimap/Qualimap_LaneMerged_${i}/qualimapReport.html $WORKING_FOLDER/multiqc_qualimap/qualimapReport_${i}.html
+OUTPUT=$WORKING_FOLDER/qualimap_multi
 
 #--------------------------------------------------------------------------------
 
-# Move to working directory
-cd $WORKING_FOLDER/multiqc_qualimap
-
-# Run multiqc in the directory where all of the individual fastqc outputs are
-multiqc . -n multiqc_report_bams.html -o .
-
-conda deactivate
+# Assess quality of all bam files
+$qualimap multi-bamqc \
+-d $GUIDE_FILE \
+-outdir $OUTPUT
