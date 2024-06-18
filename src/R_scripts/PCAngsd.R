3d755f6e45dff664f1c62bd6a6fb93f79a039217
Emily Longman <Emily.Longman@uvm.edu>
2024-06-18 11:49:23 -0400

    Update PCAngsd.R

--- src/R_scripts/PCAngsd.R
+++ src/R_scripts/PCAngsd.R
@@ -5,8 +5,10 @@ library(rprojroot)
 
 # List all files and directories below the root
 dir(find_root(has_file("README.md")))
+
+dir(find_root_file("results", criterion = has_file("README.md")))
 # Set relative path from root
-rel_path_from_root <- find_root_file("results", "pcangsd", criterion = has_file("README.md"))
+rel_path_from_root <- find_root_file("results", "output", "pcangsd", criterion = has_file("README.md"))
 
 # List files in this folder to make sure you're in the right spot.
 list.files(rel_path_from_root)
