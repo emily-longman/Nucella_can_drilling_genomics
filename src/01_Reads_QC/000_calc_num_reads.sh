fcf116b98b90d64ac9ad0e3eed159c53227f2136
Emily Longman <Emily.Longman@uvm.edu>
2024-06-28 16:37:02 -0400

    Update 000_calc_num_reads.sh

--- src/01_Reads_QC/000_calc_num_reads.sh
+++ src/01_Reads_QC/000_calc_num_reads.sh
@@ -37,6 +37,10 @@ SHORT_READS_DIR=/netfiles/pespenilab_share/Nucella/raw/Shortreads
 
 Test_run=$SHORT_READS_DIR/Test_run/071023-x-fc1-L2-I8I8-18048-172-07172023-HS-393458067/071023-x-fc1-L2-I8I8-18048-172-07172023-682298951/071023-x-fc1-ds.1dacbba9ea27424790a923cf94e69163/071023-x-fc1-L2-18048-172-07172023
 
+RUN_L7=$SHORT_READS_DIR/082123-XP-fc1-L7-I8I8-18048-181-396223829/082123-XP-fc1-L7-I8I8-18048-181-688416898/082123-XP-fc1-ds.0852b8f70cc6420995dc0100cb059460/082123-XP-fc1-L7-18048-181-08232023
+
+RUN_L8=$SHORT_READS_DIR/082123-XP-fc1-L8-I8I8-18048-181-396225832/082123-XP-fc1-L8-I8I8-18048-181-688550906/082123-XP-fc1-ds.666cbe3e3df14e29a6313b5959b79319/082123-XP-fc1-L8-18048-181-08232023
+
 #--------------------------------------------------------------------------------
 
 cd /gpfs2/scratch/elongman/Nucella_can_drilling_genomics/data/processed
@@ -52,9 +56,9 @@ fi
 
 Output_dir=/gpfs2/scratch/elongman/Nucella_can_drilling_genomics/data/processed/General_stats
 
-cd $Test_run
+# Calculate the number of reads for R1 and R2 in Test run
 
-# Calculate the number of reads for R1 in Test run
+cd $Test_run
 
 {
 for i in *R1*.gz; do
@@ -67,3 +71,37 @@ for i in *R2*.gz; do
 echo $(cat $i|wc -l)/4|bc
 done 
 } > $Output_dir/Test_run_summary.R2  
+
+
+
+# Calculate the number of reads for R1 and R2 in Run L7
+
+cd $RUN_L7
+
+{
+for i in *R1*.gz; do
+echo $(cat $i|wc -l)/4|bc
+done 
+} > $Output_dir/RUN_L7_summary.R1  
+
+{
+for i in *R2*.gz; do
+echo $(cat $i|wc -l)/4|bc
+done 
+} > $Output_dir/RUN_L7_summary.R2  
+
+# Calculate the number of reads for R1 and R2 in Run L8
+
+cd $RUN_L8
+
+{
+for i in *R1*.gz; do
+echo $(cat $i|wc -l)/4|bc
+done 
+} > $Output_dir/RUN_L8_summary.R1  
+
+{
+for i in *R2*.gz; do
+echo $(cat $i|wc -l)/4|bc
+done 
+} > $Output_dir/RUN_L8_summary.R2  
