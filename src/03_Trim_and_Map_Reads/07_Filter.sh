51bc3f8746fdc0b232c1a91fdc78a396cc156272
Emily Longman <Emily.Longman@uvm.edu>
2024-07-16 12:55:23 -0400

    Work on filtering bams

---
+++ src/03_Trim_and_Map_Reads/07_Filter.sh
@@ -0,0 +1,155 @@
+#!/usr/bin/env bash
+
+# In the command line, run the following command: sbatch path/to/this/file.sh
+
+# Request cluster resources ----------------------------------------------------
+
+# Name this job
+#SBATCH --job-name=Filter_bams
+
+# Specify partition
+#SBATCH --partition=bluemoon
+
+# Request nodes
+#SBATCH --nodes=1 
+#SBATCH --ntasks-per-node=1
+
+# Reserve walltime -- hh:mm:ss --30 hrs max
+#SBATCH --time=24:00:00 
+
+# Request memory for the entire job -- you can request --mem OR --mem-per-cpu
+#SBATCH --mem=60G 
+
+# Submit job array
+#SBATCH --array=1-576%20
+
+# Name output of this job using %x=job-name and %j=job-id
+#SBATCH --output=./slurmOutput/Filter_Bams.%A_%a.out # Standard output
+
+# Receive emails when job begins and ends or fails
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=emily.longman@uvm.edu 
+
+#--------------------------------------------------------------------------------
+
+#Load modules 
+module load samtools-1.10-gcc-7.3.0-pdbkohx
+qualimap=/netfiles/nunezlab/Shared_Resources/Software/qualimap_v2.2.1/qualimap
+
+# Define important file locations
+
+#Working folder is core folder where this pipeline is being run.
+WORKING_FOLDER=/gpfs2/scratch/elongman/Nucella_can_drilling_genomics/data/processed/fastq_to_VCF
+
+#This is a unique number id which identifies this run
+unique_run_id=`date +%N`
+
+#Name of pipeline
+PIPELINE=Filter_Bams
+
+#--------------------------------------------------------------------------------
+# Define parameters
+CPU=$SLURM_CPUS_ON_NODE
+echo "using #CPUs ==" $SLURM_CPUS_ON_NODE
+QUAL=40 # Quality threshold for samtools
+JAVAMEM=18G # Java memory
+
+#--------------------------------------------------------------------------------
+
+## PREPARE GUIDE FILES
+## Read guide files
+# This is a file with the name all the samples to be processed. One sample name per line with all the info.
+
+SAMPLE_FILE=/gpfs2/scratch/elongman/Nucella_can_drilling_genomics/data/processed/fastq_to_VCF/GuideFileMerged.txt
+
+#Example: -- the headers are just for descriptive purposes. The actual file has no headers.
+## Snail_ID  Sample#   Merged_name 1    Merged_name 2    Merged_name 3   Merged_bam_name
+##  FB1-1     S84     FB1-1_S84_L002   FB1-1_S84_L007   FB1-1_S84_L008     FB1-1_S84
+##  FB1-2     S173    FB1-2_S173_L002  FB1-2_S173_L007  FB1-2_S173_L008    FB1-2_S173
+##  FB1-5     S109    FB1-5_S109_L002  FB1-5_S109_L007  FB1-5_S109_L008    FB1-5_S109
+##  ...
+##  MP9-9     S191    MP9-9_S191_L002  MP9-9_S191_L007  MP9-9_S191_L008    MP9-9_S191
+##  MP9-10    S26     MP9-10_S26_L002  MP9-10_S26_L007  MP9-10_S26_L008    MP9-10_S26
+
+#--------------------------------------------------------------------------------
+
+# Determine sample to process, "i" and read files
+i=`awk -F "\t" '{print $6}' $SAMPLE_FILE | sed "${SLURM_ARRAY_TASK_ID}q;d"`
+echo $i
+
+#--------------------------------------------------------------------------------
+
+# Move to working directory
+cd $WORKING_FOLDER
+
+# Begin Pipeline
+
+# This part of the pipeline will generate log files to record warnings and completion status
+
+# Welcome message
+echo "Your unique run id is:" $unique_run_id
+
+echo $PIPELINE
+echo $WORKING_FOLDER
+
+cd Logs
+
+if [[ -e "${PIPELINE}.warnings.log" ]]
+then echo "Warning log exist"; echo "Let's move on."; date
+else echo "Warning log doesnt exist. Let's fix that."; touch $WORKING_FOLDER/Logs/${PIPELINE}.warnings.log; date
+fi
+
+if [[ -e "${PIPELINE}.completion.log" ]]
+then echo "Completion log exist"; echo "Let's move on."; date
+else echo "Completion log doesnt exist. Let's fix that."; touch $WORKING_FOLDER/Logs/${PIPELINE}.completion.log; date
+fi
+
+#--------------------------------------------------------------------------------
+
+# Move to working directory
+cd $WORKING_FOLDER
+
+# Generate Folders and files
+
+# This part of the script will check and generate, if necessary, all of the output folders used in the script
+
+if [ -d "Filtered_Bams" ]
+then echo "Working Filtered_Bams folder exist"; echo "Let's move on."; date
+else echo "Working Filtered_Bams folder doesnt exist. Let's fix that."; mkdir $WORKING_FOLDER/Filtered_Bams; date
+fi
+
+if [ -d "Filtered_Bams_qualimap" ]
+then echo "Working Filtered_Bams_qualimap folder exist"; echo "Let's move on."; date
+else echo "Working Filtered_Bams_qualimap folder doesnt exist. Let's fix that."; mkdir $WORKING_FOLDER/Filtered_Bams_qualimap; date
+fi
+
+#--------------------------------------------------------------------------------
+
+# Filter merged bam files with samtools view and add flags
+# -q = Skip alignments with MAPQ smaller than $QUAL (40)
+# 0x0002 = read mapped in proper pair (0x2)*
+# 0x0004 = read unmapped (0x4)
+# 0x0008 = mate unmapped (0x8)*
+
+samtools view \
+-b \
+-q $QUAL \
+-f 0x0002 -F 0x0004 -F 0x0008 \
+--threads $CPU  \
+$WORKING_FOLDER/Merged_Bams/${i}.Lanes_merged.bam > $WORKING_FOLDER/Filtered_Bams/${i}.flt.bam
+
+
+# Assess quality of final file
+$qualimap bamqc \
+-bam $WORKING_FOLDER/Filtered_Bams/${i}.flt.bam \
+-outdir $WORKING_FOLDER/Merged_Bams_qualimap/Qualimap_LaneMerged_${i} \
+--java-mem-size=$JAVAMEM
+
+#--------------------------------------------------------------------------------
+# Inform that sample is done
+
+# This part of the pipeline will notify the completion of run i. 
+
+echo ${i} " completed" >> $WORKING_FOLDER/${PIPELINE}.completion.log
+
+echo "pipeline completed" $(date)
---
+++ src/05_PCA/06_Genotype_Likelihoods_SNPs_flt.sh
@@ -0,0 +1,166 @@
+#!/usr/bin/env bash
+
+# In the command line, run the following command: sbatch path/to/this/file.sh
+
+# Request cluster resources ----------------------------------------------------
+
+# Name this job
+#SBATCH --job-name=Genotype_likelihoods_SNPs_flt
+
+# Specify partition
+#SBATCH --partition=bigmemwk
+
+# Request nodes
+#SBATCH --nodes=1 
+#SBATCH --ntasks-per-node=1
+
+# Reserve walltime -- hh:mm:ss --7 day limit 
+#SBATCH --time=06-00:00:00 
+
+# Request memory for the entire job -- you can request --mem OR --mem-per-cpu
+#SBATCH --mem=500G 
+
+# Name output of this job using %x=job-name and %j=job-id
+#SBATCH --output=./slurmOutput/%x_%j.out # Standard output
+
+# Receive emails when job begins and ends or fails
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=emily.longman@uvm.edu 
+
+#--------------------------------------------------------------------------------
+
+
+#Load modules 
+module load angsd-0.933-gcc-7.3.0-4wsdzjw
+module load samtools-1.10-gcc-7.3.0-pdbkohx
+
+# Define important file locations
+
+#Working folder is core folder where this pipeline is being run.
+WORKING_FOLDER=/gpfs2/scratch/elongman/Nucella_can_drilling_genomics/data/processed/fastq_to_VCF
+
+#Folder for joint bams
+BAMS_FOLDER=/gpfs2/scratch/elongman/Nucella_can_drilling_genomics/data/processed/fastq_to_VCF/Filtered_Bams
+
+#This is the location where the reference genome and all its indexes are stored.
+REFERENCE=/netfiles/pespenilab_share/Nucella/processed/Base_Genome/Base_Genome_May2024/Assembly.fasta.k24.w150.z1000.ntLink.8rounds.fa
+
+#Name of pipeline
+PIPELINE=Genotype_Likelihoods_SNPs_flt
+
+#--------------------------------------------------------------------------------
+# Define parameters
+CPU=$SLURM_CPUS_ON_NODE
+echo "using #CPUs ==" $SLURM_CPUS_ON_NODE
+
+#--------------------------------------------------------------------------------
+# Move to working directory
+cd $WORKING_FOLDER
+
+# Begin Pipeline
+
+# This part of the pipeline will generate log files to record warnings and completion status
+
+# Welcome message
+echo "Your unique run id is:" $unique_run_id
+
+echo $PIPELINE
+echo $WORKING_FOLDER
+
+cd Logs
+
+if [[ -e "${PIPELINE}.warnings.log" ]]
+then echo "Warning log exist"; echo "Let's move on."; date
+else echo "Warning log doesnt exist. Let's fix that."; touch $WORKING_FOLDER/${PIPELINE}.warnings.log; date
+fi
+
+if [[ -e "${PIPELINE}.completion.log" ]]
+then echo "Completion log exist"; echo "Let's move on."; date
+else echo "Completion log doesnt exist. Let's fix that."; touch $WORKING_FOLDER/${PIPELINE}.completion.log; date
+fi
+
+#--------------------------------------------------------------------------------
+
+# Move to working directory
+cd $WORKING_FOLDER
+
+# Generate Folders and files
+
+# This part of the script will check and generate, if necessary, all of the output folders used in the script
+
+if [ -d "genotype_likelihoods_SNPs_flt" ]
+then echo "Working genotype_likelihoods_SNPs_flt folder exist"; echo "Let's move on."; date
+else echo "Working genotype_likelihoods_SNPs_flt folder doesnt exist. Let's fix that."; mkdir $WORKING_FOLDER/genotype_likelihoods_SNPs_flt; date
+fi
+
+#Output folder
+OUTPUT=$WORKING_FOLDER/genotype_likelihoods_SNPs_flt
+
+#--------------------------------------------------------------------------------
+
+## PREPARE bamlist
+# This is a file with the name and full path of all the bam files to be processed.
+
+# Move to bams folder
+cd $BAMS_FOLDER
+
+# Create bamlist for all Nucella samples
+ls -d "$PWD/"* > $OUTPUT/Nucella_bam.list
+
+#--------------------------------------------------------------------------------
+
+# Estimate Genotype Likelihoods's and allele frequencies for only the polymorphic sites
+
+# Move back to working directory
+cd $WORKING_FOLDER
+
+# File suffix to distinguish analysis choices
+SUFFIX_2="SNPs_flt"
+
+## Filter changes:
+# minMapQ: threshold for minimum read mapping quality (Phred): increase from 20 to 30
+# minInd: set min number of individuals to keep a site to 85% of 192 individuals
+# setMinDepthInd: discard individual if sequencing depth for an individual is below 0.1
+# skipTriallelic: don’t use sites with >2 alleles
+# minMaf: Keep only sites with minor allele freq > some proportion (0.01)
+
+# Generate GL's for polymorphic sites for all Nucella samples
+angsd -b ${OUTPUT}/Nucella_bam.list \
+-ref ${REFERENCE} \
+-anc ${REFERENCE} \
+-out ${OUTPUT}/Nucella_${SUFFIX_2} \
+-nThreads $CPU \
+-remove_bads 1 \
+-C 50 \
+-baq 1 \
+-minMapQ 30 \
+-minQ 20 \
+-minInd 163 \
+-setMinDepthInd 0.1 \
+-skipTriallelic 1 \
+-GL 1 \
+-doSaf 1 \
+-doCounts 1 \
+-doMajorMinor 1 \
+-doMaf 1 \
+-doGlf 2 \
+-SNP_pval 1e-6 \
+-minMaf 0.01
+
+# -nThreads 1: how many cpus to use – be conservative
+# -remove_bads 1: remove reads flagged as ‘bad’ by samtools
+# -C 50: enforce downgrading of map quality if contains excessive mismatches
+# -baq 1: estimates base alignment qualities for bases around indels
+# -minMapQ 20: threshold for minimum read mapping quality (Phred)
+# -minQ 20: threshold for minimum base quality (Phred)
+# -minInd 163: min number of individuals to keep a site
+# -setMinDepthInd 0.1: min read depth for an individual to count towards a site
+# -skipTriallelic 1: don’t use sites with >2 alleles
+# -GL 1: calculate genotype likelihoods (GL) using the Samtools formula
+# -doSaf 1: output allele frequency likelihoods for each site
+# -doCounts 1: output allele counts for each site
+# -doMajorMinor 1: fix major and minor alleles the same across all samples
+# -doMaf 1: calculate minor allele frequencies
+# -doGlf 2: gives us the Beagle format which will be used by pcangsd
+# -SNP_pval 1e-6: Keep only site highly likely to be polymorphic (SNPs)
+# -minMaf 0.01: Keep only sites with minor allele freq > some proportion.
