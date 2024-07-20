790d89facc02df5b37684963cebde1ab71a32214
Emily Longman <Emily.Longman@uvm.edu>
2024-07-20 12:15:35 -0400

    Update scripts

--- src/00_Genome_short_read/10_Partion_genome_in_chunks.R
+++ src/00_Genome_short_read/10_Partion_genome_in_chunks.R
@@ -1,9 +1,22 @@
+# Set path as main Github repo
+library(rprojroot)
+
+# List all files and directories below the root
+dir(find_root(has_file("README.md")))
+# Set relative path from root
+rel_path_from_root <- find_root_file("results", "tables", criterion = has_file("README.md"))
+
+# List files in this folder to make sure you're in the right spot.
+list.files(rel_path_from_root) 
+# Set working directory as path from root
+setwd(rel_path_from_root)
+
+
 ###
 library(foreach)
 
 # Break up the number of contigs (i.e. 27922) into 50 contig chunks
 seq(from=1, to=27922, by= 50) -> vec.num
-seq(from=1, to=88155, by= 50) -> vec.num
 
 dat.win=foreach(i=vec.num, .combine = "rbind")%do%{
   data.frame(start=i, end=i+49)
