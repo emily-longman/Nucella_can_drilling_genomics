8b4fa9951bddb36d99587854f056248ce255d1e4
Emily Longman <Emily.Longman@uvm.edu>
2024-08-04 10:25:55 -0400

    Rename optimized scripts

--- src/00_Genome_short_read/05_DBG2OLC.optimum.sh
+++ src/00_Genome_short_read/05_DBG2OLC.optimum.sh
@@ -5,17 +5,17 @@
 # Request cluster resources ----------------------------------------------------
 
 # Name this job
-#SBATCH --job-name=DBG2OLC_optimized_rerun
+#SBATCH --job-name=DBG2OLC.optimum.2_100_0.01
 
 # Specify partition
 #SBATCH --partition=bigmemwk
 
 # Request nodes
-#SBATCH --nodes=1 
+#SBATCH --nodes=3 
 #SBATCH --ntasks-per-node=1  
 
 # Reserve walltime -- hh:mm:ss --7 day limit
-#SBATCH --time=07-00:00:00 
+#SBATCH --time=03-00:00:00 
 
 # Request memory for the entire job -- you can request --mem OR --mem-per-cpu
 #SBATCH --mem=900G
@@ -31,8 +31,6 @@
 
 # DBG2OLC executable
 DBG2OLC=/gpfs1/home/e/l/elongman/software/DBG2OLC
-# Quast executable
-quast=/netfiles/nunezlab/Shared_Resources/Software/quast-5.2.0/quast.py
 
 #--------------------------------------------------------------------------------
 
@@ -43,68 +41,59 @@ WORKING_FOLDER=/netfiles/pespenilab_share/Nucella/processed/Base_Genome/short_re
 #ONT_FOLDER=/netfiles/pespenilab_share/Nucella/raw/ONT
 #If you haven't done it yet, unzip the files 
 #gunzip $ONT_FOLDER/FC_all.ONT.nuc.fastq.gz
+#If you haven't done it yet, unzip the files 
 ONT=/netfiles/pespenilab_share/Nucella/processed/Base_Genome/ONT_fltlong/Nuc.2000.fltlong.fastq
+
+# Contigs from Sparse Assembler (using the SparseAssembler with the largest N50 (SparseAssembler_101_2_1))
+Contigs=$WORKING_FOLDER/SparseAssembler/SparseAssembler_101_2_1/Contigs.txt
+
+# Extract base files from previous DBG2OLC run
+compressed_ONT=$WORKING_FOLDER/DBG2OLC/DBG2OLC_2_20_0.015_rerun/ReadsInfoFrom_Nuc.2000.fltlong.fastq
 
 #--------------------------------------------------------------------------------
 
-# Determine parameter combination to process
-AT=0.01
-MO=100
-kCT=2
+# Parameter combinations
+# kmerCovTh = 2
+# MinOverlap = 100
+# AdaptiveTh = 0.01
 
-label=DBG2OLC_KmC_${kCT}_MinOv_${MO}_Adth_${AT}
+A=0.01
+M=100
+K=2
 
-echo ${label} ${kCT} ${MO} ${AT}
+label=KmC_${K}_MinOv_${M}_Adth_${A}
+
+echo ${K} ${M} ${A}
+
+echo ${label}
 
 #--------------------------------------------------------------------------------
 
 cd $WORKING_FOLDER/DBG2OLC
 
 # Make directory for each DBG2OLC parameter combination
-if [ -d "${label}" ]
-then echo "Working ${label} folder exist"; echo "Let's move on."; date
-else echo "Working ${label} folder doesnt exist. Let's fix that."; mkdir $WORKING_FOLDER/DBG2OLC/${label}; date
-fi
-
-# Make Quast directory for each parameter combination
-cd $WORKING_FOLDER/DBG2OLC/Quast
-if [ -d "${label}" ]
-then echo "Working ${label} folder exist"; echo "Let's move on."; date
-else echo "Working ${label} folder doesnt exist. Let's fix that."; mkdir $WORKING_FOLDER/DBG2OLC/Quast/${label}; date
+if [ -d "DBG2OLC_${label}" ]
+then echo "Working DBG2OLC_${label} folder exist"; echo "Let's move on."; date
+else echo "Working DBG2OLC_${label} folder doesnt exist. Let's fix that."; mkdir $WORKING_FOLDER/DBG2OLC/DBG2OLC_${label}; date
 fi
 
 #--------------------------------------------------------------------------------
 
-# Move to the directory where the output files will be saved
-cd $WORKING_FOLDER/DBG2OLC/${label}
+# Use DBG2OLC to construct short but accurate contigs  
 
-# Using the SparseAssembler with the largest N50 (SparseAssembler_101_2_1)
-# k= 101, NodeCovTh = 2, EdgeCovTh =1 (the last parameter didn't have an effect)
+# Move to the directory where the output files will be saved
+cd $WORKING_FOLDER/DBG2OLC/DBG2OLC_${label}
+
+# Copy read information from previous run
+cp $compressed_ONT ./
 
 # Use DBG2OLC to construct short but accurate contigs  
 $DBG2OLC \
+LD 1 \
 k 17 \
-AdaptiveTh ${AT} \
-KmerCovTh ${kCT} \
-MinOverlap ${MO} \
+AdaptiveTh ${A} \
+KmerCovTh ${K} \
+MinOverlap ${M} \
 RemoveChimera 1 \
-Contigs $WORKING_FOLDER/SparseAssembler/SparseAssembler_101_2_1/Contigs.txt \
+Contigs $Contigs \
 f $ONT
-
-#--------------------------------------------------------------------------------
-
-# Rename output backbone file with label name
-
-echo "renaming outputs"
-
-mv backbone_raw.fasta ${label}.backbone_raw.fasta
-
-#--------------------------------------------------------------------------------
-
-# Run quast
-$quast $WORKING_FOLDER/DBG2OLC/${label}/${label}.backbone_raw.fasta \
--o $WORKING_FOLDER/DBG2OLC/Quast/${label}
-
-#--------------------------------------------------------------------------------
-
-echo "done"
