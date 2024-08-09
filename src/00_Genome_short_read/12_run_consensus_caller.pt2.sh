94415f787b0539c21e6737824fab49bc01c2e74f
Emily Longman <Emily.Longman@uvm.edu>
2024-08-08 22:19:27 -0400

    Update 12_run_consensus_caller.pt2.sh

--- src/00_Genome_short_read/12_run_consensus_caller.pt2.sh
+++ src/00_Genome_short_read/12_run_consensus_caller.pt2.sh
@@ -15,7 +15,7 @@
 #SBATCH --ntasks-per-node=1  
 
 # Reserve walltime -- hh:mm:ss 
-#SBATCH --time=28:00:00 
+#SBATCH --time=8:00:00 
 
 # Request memory for the entire job -- you can request --mem OR --mem-per-cpu
 #SBATCH --mem=700G
