35c1b0e3f8e461c956b241612b8fa3171a40de5c
Emily Longman <Emily.Longman@uvm.edu>
2024-08-22 10:58:30 -0400

    Update split_and_run_sparc.pt2.array.sh

--- src/00_Genome_short_read/12_consensus_scripts_extra/split_and_run_sparc.pt2.array.sh
+++ src/00_Genome_short_read/12_consensus_scripts_extra/split_and_run_sparc.pt2.array.sh
@@ -4,7 +4,7 @@
 # USAGE: ./split_and_run_sparc.sh [BACKBONE_FASTA] [CONSENSUS_FASTA] [READS_FASTA] [OUTPUT_DIR] [ITERATIONS] ###
 
 backbone_fasta=$1 #backbone 
-consensus_fasta=$2 #text file with contig names in chunks
+consensus_fasta=$2 #consensus info
 reads_fasta=$3 #contigs and raw ONT reads catted together
 split_dir=$4 #file output directory 
 iterations=$5 #set to 2
@@ -37,27 +37,23 @@ Sparc=/gpfs1/home/e/l/elongman/software/Sparc
 
 #===#/gpfs2/scratch/elongman/Nucella_can_drilling_genomics/src/00_Genome_short_read/12_consensus_scripts_extra/split_reads_by_backbone.py -b ${backbone_fasta} -o ${split_dir} -r ${reads_fasta} -c ${consensus_fasta} -s ${nshift}
 
-for file in $(find ${split_dir} -name "*.reads.fasta"); do
-chunk=`basename $file .reads.fasta`
+for file in $(cat backbone.names.${SLURM_ARRAY_TASK_ID}.txt); do 
+echo $file
 
 cmd=""
 for iter in `seq 1 ${iterations}`; do
 
-d="${split_dir}/${chunk}"
-
 echo $iter
-echo $chunk
-echo $d
-echo "Align contig with blasr and Sparc."
+echo "Align backbone with blasr and Sparc."
 
-blasr -nproc $ncpus ${split_dir}/${chunk}.reads.fasta ${split_dir}/${chunk}.fasta -bestn 1 -m 5 -minMatch 19 -out $WORKING_FOLDER_SCRATCH/consensus/${chunk}.mapped.m5   
-$Sparc m $WORKING_FOLDER_SCRATCH/consensus/${chunk}.mapped.m5 b ${split_dir}/${chunk}.fasta k 1 c 2 g 1 HQ_Prefix Contig boost 5 t 0.2 o $WORKING_FOLDER_SCRATCH/consensus/${chunk}
+blasr -nproc $ncpus ${split_dir}/${file}.reads.fasta ${split_dir}/${file}.fasta -bestn 1 -m 5 -minMatch 19 -out $WORKING_FOLDER_SCRATCH/consensus/${file}.mapped.m5   
+$Sparc m $WORKING_FOLDER_SCRATCH/consensus/${file}.mapped.m5 b ${split_dir}/${file}.fasta k 1 c 2 g 1 HQ_Prefix Contig boost 5 t 0.2 o $WORKING_FOLDER_SCRATCH/consensus/${file}
 
 if [[ ${iter} -lt ${iterations} ]]
 then
 
 # Rename and move to final assembly directory
-cmd="mv $WORKING_FOLDER_SCRATCH/consensus/${chunk}.consensus.fasta $WORKING_FOLDER_SCRATCH/consensus/final_assembly_no_array/${chunk}.consensus.fasta"
+cmd="mv $WORKING_FOLDER_SCRATCH/consensus/${file}.consensus.fasta $WORKING_FOLDER_SCRATCH/consensus/final_assembly_array/${file}.consensus.fasta"
 echo $cmd ; eval $cmd
 fi
 done
@@ -65,9 +61,8 @@ done
 echo $cmd
 eval $cmd
 
-
 #to save space
-cmd="rm ${chunk}.mapped.m5"
+cmd="rm ${file}.mapped.m5"
 echo $cmd
 eval $cmd
 #cmd="rm ${split_dir}/${chunk}.reads.fasta"
