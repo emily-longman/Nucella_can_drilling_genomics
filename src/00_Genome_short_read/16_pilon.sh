5e210f031f609c39888128318b09d3e4221c7ca2
Emily Longman <Emily.Longman@uvm.edu>
2024-08-26 12:42:39 -0400

    Create 16_pilon.sh

---
+++ src/00_Genome_short_read/16_pilon.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+# In the command line, run the following command: sbatch path/to/this/file.sh
+
+# Request cluster resources ----------------------------------------------------
+
+# Name this job
+#SBATCH --job-name=busco_ntlink
+
+# Specify partition
+#SBATCH --partition=bluemoon
+
+# Request nodes
+#SBATCH --nodes=1 # on one node
+#SBATCH --ntasks-per-node=1  
+
+# Request CPUs per task
+#SBATCH -c 1
+
+# Reserve walltime -- hh:mm:ss --30 hour limit
+#SBATCH --time=30:00:00 
+
+# Request memory for the entire job -- you can request --mem OR --mem-per-cpu
+#SBATCH --mem=60G
+
+# Submit job array
+#SBATCH --array=1-2
+
+# Name output of this job using %x=job-name and %j=job-id
+#SBATCH --output=./slurmOutput/busco.%A_%a.out # Standard output
+
+# Receive emails when job begins and ends or fails
+#SBATCH --mail-type=ALL # indicates if you want an email when the job starts, ends, or both
+#SBATCH --mail-user=emily.longman@uvm.edu # where to email updates to
+
+#--------------------------------------------------------------------------------
+
+
+#--------------------------------------------------------------------------------
+
+# Call package (installed with conda)
+module load python3.11-anaconda/2023.09-0
+source ${ANACONDA_ROOT}/etc/profile.d/conda.sh
+#conda create --name pilon #create and name the environment
+conda activate pilon #activate the environment
+#conda install bioconda::pilon # install the program
+
+#--------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+#--------------------------------------------------------------------------------
+
+conda deactivate
