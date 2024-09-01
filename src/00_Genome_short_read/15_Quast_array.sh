c1aba1774a3c7f26ef8916d4f5975c66ae0917bf
Emily Longman <Emily.Longman@uvm.edu>
2024-09-01 08:15:52 -0400

    Update 15_Quast_array.sh

--- src/00_Genome_short_read/15_Quast_array.sh
+++ src/00_Genome_short_read/15_Quast_array.sh
@@ -92,7 +92,7 @@ fi
 #--------------------------------------------------------------------------------
 
 # Assembly name
-ASSEMBLY=$WORKING_FOLDER_SCRATCH/ntlink/ntlink_k.${k}_w.${w}/final_assembly.fasta.k${k}.w${w}.z1000.ntLink.ntLink.ntLink.ntLink.ntLink.gap_fill.fa.k${k}.w${w}.z1000.ntLink.scaffolds.gap_fill.fa
+ASSEMBLY=$WORKING_FOLDER_SCRATCH/ntlink/ntlink_${label}/final_assembly.fasta.k${k}.w${w}.z1000.ntLink.ntLink.ntLink.ntLink.ntLink.gap_fill.fa.k${k}.w${w}.z1000.ntLink.scaffolds.gap_fill.fa
 
 # Run quast
 $quast $ASSEMBLY \
