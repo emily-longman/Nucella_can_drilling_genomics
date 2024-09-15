2a1a2d1c9ee3434c38bce9ff869a290b1e2992a8
Emily Longman <Emily.Longman@uvm.edu>
2024-09-15 08:55:33 -0400

    Create 17_A_pilon_index_genome.sh

---
+++ src/00_Genome_short_read/17_A_pilon_index_genome.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+
+# In the command line, run the following command: sbatch path/to/this/file.sh
+
+# Request cluster resources ----------------------------------------------------
+
+# Name this job
+#SBATCH --job-name=index_genome
+
+# Specify partition
+#SBATCH --partition=bluemoon
+
+# Request nodes
+#SBATCH --nodes=1 
+#SBATCH --ntasks-per-node=1
+
+# Reserve walltime -- hh:mm:ss --30 hrs max
+#SBATCH --time=24:00:00 
+
+# Request memory for the entire job -- you can request --mem OR --mem-per-cpu
+#SBATCH --mem=60G 
+
+# Name output of this job using %x=job-name and %j=job-id
+#SBATCH --output=./slurmOutput/%x_%j.out # Standard output
+
+# Receive emails when job begins and ends or fails
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=emily.longman@uvm.edu 
+
+#--------------------------------------------------------------------------------
+
+# This script with index the reference genome.
+
+# Note: this step only needs to be done once
+
+#Load modules 
+spack load samtools@1.10
+bwa=/netfiles/nunezlab/Shared_Resources/Software/bwa-mem2-2.2.1_x64-linux/bwa-mem2.avx2
+PICARD=/netfiles/nunezlab/Shared_Resources/Software/picard/build/libs/picard.jar
+
+#--------------------------------------------------------------------------------
+
+#Define important file locations
+
+# Working folder is core folder where this pipeline is being run.
+WORKING_FOLDER_SCRATCH=/gpfs2/scratch/elongman/Nucella_can_drilling_genomics/data/processed/short_read_assembly
+
+# Genome from first round of pilon
+ASSEMBLY=$WORKING_FOLDER_SCRATCH/pilon/polished_genome_round_1/polished_assembly.fasta
+
+#--------------------------------------------------------------------------------
+
+# Move to the directory that the genome is currently stored
+cd $WORKING_FOLDER_SCRATCH/pilon/polished_genome_round_1
+
+# Index database sequences in the FASTA format 
+$bwa index $ASSEMBLY 
+
+# Generate the FASTA sequence dictionary file
+java -jar $PICARD CreateSequenceDictionary \
+R=$ASSEMBLY
+
+# Generate the fasta index file for the reference
+samtools faidx $ASSEMBLY
