1ec4e24f847a0341f81ca9fcb5b704ad8ba1e84b
Emily Longman <Emily.Longman@uvm.edu>
2024-09-19 15:31:00 -0400

    Update 02_pcangsd.sh

--- src/01_Fastq_to_GL/03_Call_SNPs/02_pcangsd.sh
+++ src/01_Fastq_to_GL/03_Call_SNPs/02_pcangsd.sh
@@ -95,14 +95,14 @@ OUTPUT=$WORKING_FOLDER/pcangsd
 #--------------------------------------------------------------------------------
 
 # Make a copy of the list of bam files for all the Nucella samples and place in the output directory. You'll need this later for making figures.
-cp ${INPUT}/Nucella_bam.list ${OUTPUT}
+cp ${INPUT}/Nucella_bam_test.list ${OUTPUT}
 
 # Then, run PCA and admixture scores with pcangsd:
 SUFFIX="Nucella_poly_covmatrix"
 
 echo "Analyse covariance matrix on all individuals"
 
-pcangsd -b ${INPUT}/Nucella_SNPs_all.beagle.gz \
+pcangsd -b ${INPUT}/Nucella_SNPs_all_test.beagle.gz \
 -o ${OUTPUT}/${SUFFIX} \
 -t $CPU 
 
