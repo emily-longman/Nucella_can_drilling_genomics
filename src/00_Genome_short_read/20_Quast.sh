95d1717e57f3667bd5770a34671d5910ecdcd9f7
Emily Longman <Emily.Longman@uvm.edu>
2024-10-03 10:50:21 -0400

    Update quast script

---
+++ src/00_Genome_short_read/20_Quast.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+# In the command line, run the following command: sbatch path/to/this/file.sh
+
+# Request cluster resources ----------------------------------------------------
+
+# Name this job
+#SBATCH --job-name=Quast_pilon
+
+# Specify partition
+#SBATCH --partition=bluemoon
+
+# Request nodes
+#SBATCH --nodes=1 
+#SBATCH --ntasks-per-node=1  
+
+# Reserve walltime -- hh:mm:ss --7 day limit
+#SBATCH --time=3:00:00 
+
+# Request memory for the entire job -- you can request --mem OR --mem-per-cpu
+#SBATCH --mem=50G
+
+# Name output of this job using %x=job-name and %j=job-id
+#SBATCH --output=./slurmOutput/%x_%j.out # Standard output
+
+# Receive emails when job begins and ends or fails
+#SBATCH --mail-type=ALL # indicates if you want an email when the job starts, ends, or both
+#SBATCH --mail-user=emily.longman@uvm.edu # where to email updates to
+
+#--------------------------------------------------------------------------------
+
+# This script will run Quast on the final consensus assembly 
+
+# Quast executable
+quast=/netfiles/nunezlab/Shared_Resources/Software/quast-5.2.0/quast.py
+
+#--------------------------------------------------------------------------------
+
+#Working folder is core folder where this pipeline is being run.
+WORKING_FOLDER_SCRATCH=/gpfs2/scratch/elongman/Nucella_can_drilling_genomics/data/processed/short_read_assembly
+
+#--------------------------------------------------------------------------------
+
+cd $WORKING_FOLDER_SCRATCH/pilon/polished_genome_round_5
+
+# Make Quast directory 
+if [ -d "Quast" ]
+then echo "Working Quast folder exist"; echo "Let's move on."; date
+else echo "Working Quast folder doesnt exist. Let's fix that."; mkdir $WORKING_FOLDER_SCRATCH/pilon/polished_genome_round_5/Quast; date
+fi
+
+#--------------------------------------------------------------------------------
+
+# Run quast
+$quast $WORKING_FOLDER_SCRATCH/pilon/polished_genome_round_5/polished_assembly.fasta \
+-o $WORKING_FOLDER_SCRATCH/pilon/polished_genome_round_5/Quast
