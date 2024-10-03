6b04ac2677f4d5c5ef148430e3b14074e6e72474
Emily Longman <Emily.Longman@uvm.edu>
2024-10-03 09:38:56 -0400

    Update 21_C_cat_scaffolds.sh

---
+++ src/00_Genome_short_read/21_C_cat_scaffolds.sh
@@ -0,0 +1,46 @@
+
+#!/usr/bin/env bash
+
+# In the command line, run the following command: sbatch path/to/this/file.sh
+
+# Request cluster resources ----------------------------------------------------
+
+# Name this job
+#SBATCH --job-name=cat_scaffolds
+
+# Specify partition
+#SBATCH --partition=bluemoon
+
+# Request nodes
+#SBATCH --nodes=1 
+#SBATCH --ntasks-per-node=1  
+
+# Reserve walltime -- hh:mm:ss 
+#SBATCH --time=2:00:00 
+
+# Request memory for the entire job -- you can request --mem OR --mem-per-cpu
+#SBATCH --mem=30G
+
+# Name output of this job using %x=job-name and %j=job-id
+#SBATCH --output=./slurmOutput/%x_%j.out # Standard output
+
+# Receive emails when job begins and ends or fails
+#SBATCH --mail-type=ALL # indicates if you want an email when the job starts, ends, or both
+#SBATCH --mail-user=emily.longman@uvm.edu # where to email updates to
+
+#--------------------------------------------------------------------------------
+
+#Define important file locations
+
+# Working folder is core folder where this pipeline is being run.
+WORKING_FOLDER_SCRATCH=/gpfs2/scratch/elongman/Nucella_can_drilling_genomics/data/processed/short_read_assembly
+
+#--------------------------------------------------------------------------------
+
+scaffolds_dir=$WORKING_FOLDER_SCRATCH/rename_scaffolds/scaffolds
+
+# Cat files together to produce a final genome with the scaffolds renamed
+for file in $(find ${scaffolds_dir} -name "*.fasta"); do
+cmd="cat ${file};"
+eval $cmd
+done > $WORKING_FOLDER_SCRATCH/rename_scaffolds/N.canaliculata_assembly.fasta
