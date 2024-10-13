37ebccda6a9b56cab48f7e5637be5e3e5cd54610
Emily Longman <Emily.Longman@uvm.edu>
2024-10-13 17:35:17 -0400

    Update thermofly

--- src/Thermofly/2_Map/3_Map.sh
+++ src/Thermofly/2_Map/3_Map.sh
@@ -8,6 +8,8 @@
 #SBATCH -o ./slurmOutput/%x.%A_%a.out  
 #SBATCH -p bluemoon  
 #SBATCH --array=1-22
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=emily.longman@uvm.edu 
 
 #--------------------------------------------------------------------------------
 
