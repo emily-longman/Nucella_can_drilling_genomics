786b92cafe71f66fd0551ebfb8987b5bb5957eb2
Emily Longman <Emily.Longman@uvm.edu>
2024-10-18 16:46:44 -0400

    Update 2_SAF_Olaa_array.sh

--- src/Thermofly/5_Fst/2_SAF_Olaa_array.sh
+++ src/Thermofly/5_Fst/2_SAF_Olaa_array.sh
@@ -7,7 +7,7 @@
 #SBATCH --mem 40G   
 #SBATCH --output=./slurmOutput/%x_%j.out 
 #SBATCH -p bluemoon 
-#SBATCH --array=0-99
+#SBATCH --array=0-99%10
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=emily.longman@uvm.edu 
 
