f0e4f0c2fe86915b4fabef292ea9c00b5ad58f76
Emily Longman <Emily.Longman@uvm.edu>
2024-10-18 17:09:57 -0400

    Update 3_SFS.sh

--- src/Thermofly/5_Fst/3_SFS.sh
+++ src/Thermofly/5_Fst/3_SFS.sh
@@ -5,9 +5,9 @@
 #SBATCH -N 1 # on one node  
 #SBATCH -t 8:00:00   
 #SBATCH --mem 40G   
-#SBATCH --output=./slurmOutput/%x_%j.out 
+#SBATCH -o ./slurmOutput/%x.%A_%a.out
 #SBATCH -p bluemoon 
-# Submit job array
+#SBATCH --array=0-99%10
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=emily.longman@uvm.edu 
 
@@ -22,13 +22,20 @@ spack load angsd@0.933
 
 # Set folders and file locations
 working_folder=/gpfs2/scratch/elongman/Nucella_can_drilling_genomics/data/processed/Thermofly
-meta=$working_folder/METADATA/Thermofly_metadata.tsv
-ref=/netfiles/thermofly/GENOMES/basisetae/D.basisetae_nanopore.fasta.masked
 
 #--------------------------------------------------------------------------------
 
 # Parameters for software
 CPU=6
+
+#--------------------------------------------------------------------------------
+
+# Create array
+echo ${SLURM_ARRAY_TASK_ID}
+
+array=({1..100})
+i="${array[$SLURM_ARRAY_TASK_ID]}"
+echo ${i}
 
 #--------------------------------------------------------------------------------
 
