d06fffca6c2e61ee4ce3611c62025983a5c36bc5
Emily Longman <Emily.Longman@uvm.edu>
2024-10-19 13:32:12 -0400

    Update scripts thermofly

--- src/Thermofly/5_Fst/4_Fst_R.sh
+++ src/Thermofly/5_Fst/4_Fst_R.sh
@@ -7,7 +7,7 @@
 #SBATCH --mem 40G   
 #SBATCH --output=./slurmOutput/%x_%j.out 
 #SBATCH -p bluemoon 
-# Submit job array
+#SBATCH --array=0-99%10
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=emily.longman@uvm.edu 
 
@@ -27,5 +27,17 @@ file=$working_folder/Fst/Thermofly_Tom_Olaa
 
 #--------------------------------------------------------------------------------
 
+
+# Create array
+echo ${SLURM_ARRAY_TASK_ID}
+
+array=({1..100})
+i="${array[$SLURM_ARRAY_TASK_ID]}"
+echo ${i}
+
+#--------------------------------------------------------------------------------
+
 # Call file 
+file=$working_folder/Fst/Thermofly_Tom_Olaa_subset_${i}_allsites
+
 Rscript $script_folder/5_Fst/4_Fst.R "$file"
