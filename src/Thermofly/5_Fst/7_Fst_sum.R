503b8eba860ebbf6313ca970a5dd0175fa9eb0a4
Emily Longman <Emily.Longman@uvm.edu>
2024-10-20 17:17:27 -0400

    Update 7_Fst_sum.R

--- src/Thermofly/5_Fst/7_Fst_sum.R
+++ src/Thermofly/5_Fst/7_Fst_sum.R
@@ -14,7 +14,7 @@ fst_unweighted_sd<-sd(fst_unweighted[,1])
 fst_unweighted_range<-range(fst_unweighted[,1])
 
 fst_unweighted_summary<-data.frame(fst_unweighted_mean, fst_unweighted_median, fst_unweighted_sd, fst_unweighted_range)
-write.table(fst_unweighted_summary, "fst_unweighted_summary.txt", quote=F)
+write.table(fst_unweighted_summary, "fst_unweighted_summary.txt", sep="\t", quote=F)
 
 fst_weighted_mean<-mean(fst_weighted[,1])
 fst_weighted_median<-median(fst_weighted[,1])
@@ -22,4 +22,4 @@ fst_weighted_sd<-sd(fst_weighted[,1])
 fst_weighted_range<-range(fst_weighted[,1])
 
 fst_weighted_summary<-data.frame(fst_weighted_mean, fst_weighted_median, fst_weighted_sd, fst_weighted_range)
-write.table(fst_weighted_summary, "fst_weighted_summary.txt", quote=F)
+write.table(fst_weighted_summary, "fst_weighted_summary.txt", sep="\t", quote=F)
