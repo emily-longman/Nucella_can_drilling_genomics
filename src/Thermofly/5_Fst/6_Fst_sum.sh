6bdbcb24a83442e21bd73a998024b26d86c6654e
Emily Longman <Emily.Longman@uvm.edu>
2024-10-20 12:12:30 -0400

    Update 6_Fst_sum.sh

--- src/Thermofly/5_Fst/6_Fst_sum.sh
+++ src/Thermofly/5_Fst/6_Fst_sum.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash  
 #  
-#SBATCH -J Fst_calc  
-#SBATCH -c 6  
+#SBATCH -J Fst_sum  
+#SBATCH -c 1 
 #SBATCH -N 1 # on one node  
-#SBATCH -t 8:00:00   
-#SBATCH --mem 40G   
+#SBATCH -t 1:00:00   
+#SBATCH --mem 10G   
 #SBATCH --output=./slurmOutput/%x_%j.out 
 #SBATCH -p bluemoon 
-#SBATCH --array=0-99%10
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=emily.longman@uvm.edu 
 
