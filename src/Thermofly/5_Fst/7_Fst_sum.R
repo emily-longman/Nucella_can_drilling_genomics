a82cf67ff8f325e15dff55d70c3901cfda00bf9c
Emily Longman <Emily.Longman@uvm.edu>
2024-10-20 17:10:12 -0400

    Update 7_Fst_sum.R

--- src/Thermofly/5_Fst/7_Fst_sum.R
+++ src/Thermofly/5_Fst/7_Fst_sum.R
@@ -10,14 +10,16 @@ fst_weighted<-read.table(paste0(file2))
 
 fst_unweighted_mean<-mean(fst_unweighted[,1])
 fst_unweighted_median<-median(fst_unweighted[,1])
+fst_unweighted_sd<-sd(fst_unweighted[,1])
 fst_unweighted_range<-range(fst_unweighted[,1])
 
-fst_unweighted_summary<-data.frame(fst_unweighted_mean, fst_unweighted_median, fst_unweighted_range)
+fst_unweighted_summary<-data.frame(fst_unweighted_mean, fst_unweighted_median, fst_unweighted_sd, fst_unweighted_range)
 write.table(fst_unweighted_summary, "fst_unweighted_summary.txt", quote=F)
 
 fst_weighted_mean<-mean(fst_weighted[,1])
 fst_weighted_median<-median(fst_weighted[,1])
+fst_weighted_sd<-sd(fst_weighted[,1])
 fst_weighted_range<-range(fst_weighted[,1])
 
-fst_weighted_summary<-data.frame(fst_weighted_mean, fst_weighted_median, fst_weighted_range)
+fst_weighted_summary<-data.frame(fst_weighted_mean, fst_weighted_median, fst_weighted_sd, fst_weighted_range)
 write.table(fst_weighted_summary, "fst_weighted_summary.txt", quote=F)
