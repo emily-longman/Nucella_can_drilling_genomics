5adc1dd3e7322236958e2ce6a9d002aefb13420c
Emily Longman <Emily.Longman@uvm.edu>
2024-10-27 11:25:12 -0400

    Create 01_merge_bams.sh

---
+++ src/02_Genome_Deomgraphy/01_merge_bams.sh
@@ -0,0 +1,99 @@
+#!/usr/bin/env bash
+
+# In the command line, run the following command: sbatch path/to/this/file.sh
+
+# Request cluster resources ----------------------------------------------------
+
+# Name this job
+#SBATCH --job-name=Merge_bams
+
+# Specify partition
+#SBATCH --partition=bluemoon
+
+# Request nodes
+#SBATCH --nodes=1 
+#SBATCH --ntasks-per-node=1
+
+# Reserve walltime -- hh:mm:ss --30 hrs max
+#SBATCH --time=20:00:00 
+
+# Request memory for the entire job -- you can request --mem OR --mem-per-cpu
+#SBATCH --mem=10G 
+
+# Name output of this job using %x=job-name and %j=job-id
+#SBATCH --output=./slurmOutput/%x_%j.out # Standard output
+
+# Receive emails when job begins and ends or fails
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=emily.longman@uvm.edu 
+
+#--------------------------------------------------------------------------------
+
+# This script will merge all of the bam files together. 
+
+# Load modules  
+spack load samtools@1.10
+PICARD=/netfiles/nunezlab/Shared_Resources/Software/picard/build/libs/picard.jar
+qualimap=/netfiles/nunezlab/Shared_Resources/Software/qualimap_v2.2.1/qualimap
+
+#--------------------------------------------------------------------------------
+
+# Define important file locations
+
+# Working folder is core folder where this pipeline is being run.
+WORKING_FOLDER=/gpfs2/scratch/elongman/Nucella_can_drilling_genomics/data/processed/genome_demography
+
+# GL folder is folder where the bam.
+GL_FOLDER=/gpfs2/scratch/elongman/Nucella_can_drilling_genomics/data/processed/fastq_to_GL
+
+#-------------------------------------------------------------------------------
+
+# Generate Folders and files
+
+# Move to working directory
+cd $WORKING_FOLDER
+
+# This part of the script will check and generate, if necessary, all of the output folders used in the script
+
+if [ -d "bam" ]
+then echo "Working bam folder exist"; echo "Let's move on."; date
+else echo "Working bam folder doesnt exist. Let's fix that."; mkdir $WORKING_FOLDER/bam; date
+fi
+
+#--------------------------------------------------------------------------------
+
+# I will merge all of the bam files produced in the fastq to GL pipeline into one bam file
+
+# Make temporary linefile with list of input BAM files
+ls $GL_FOLDER/bams_clean/*.srt.rmdp.bam > $WORKING_FOLDER/N.can.guide.txt
+
+
+
+
+
+
+# CONTINUE BELOW!
+
+
+
+
+# Merge the 3 sequencing lanes
+samtools merge \
+-b ${i}.guide.txt \
+$WORKING_FOLDER/bams_merged/${i}.Lanes_merged.bam
+
+# Remove the temporary guide file
+rm ${i}.guide.txt
+
+# Assess quality of final file
+$qualimap bamqc \
+-bam $WORKING_FOLDER/bams_merged/${i}.Lanes_merged.bam \
+-outdir $WORKING_FOLDER/bams_merged_qualimap/Qualimap_LaneMerged_${i} \
+--java-mem-size=$JAVAMEM
+
+#--------------------------------------------------------------------------------
+
+# Index bams with samtools
+samtools index $WORKING_FOLDER/bams_merged/${i}.Lanes_merged.bam
+
+#--------------------------------------------------------------------------------
