42f224eadaadbcd297aa85e1d06bd13cc51a5223
Emily Longman <Emily.Longman@uvm.edu>
2024-10-31 11:19:12 -0400

    Update scripts for get pruned sites

---
+++ src/01_Fastq_to_GL/05_prune_SNPs_in_LD/03_B_index_pruned_sites.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+# In the command line, run the following command: sbatch path/to/this/file.sh
+
+# Request cluster resources ----------------------------------------------------
+
+# Name this job
+#SBATCH --job-name=index_pruned_sites
+
+# Specify partition
+#SBATCH --partition=bluemoon
+
+# Request nodes
+#SBATCH --nodes=1 
+
+# Reserve walltime -- hh:mm:ss --7 day limit 
+#SBATCH --time=2:00:00 
+
+# Request memory for the entire job -- you can request --mem OR --mem-per-cpu
+#SBATCH --mem=5G 
+
+# Name output of this job using %x=job-name and %j=job-id
+#SBATCH --output=./slurmOutput/%x_%j.out # Standard output
+
+# Receive emails when job begins and ends or fails
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=emily.longman@uvm.edu 
+
+#--------------------------------------------------------------------------------
+
+# This script will index the LD-pruned SNPs with angsd.
+
+#--------------------------------------------------------------------------------
+
+#Load modules 
+spack load angsd@0.933
+
+#--------------------------------------------------------------------------------
+
+# Define important file locations
+
+# Working folder is core folder where this pipeline is being run.
+WORKING_FOLDER=/gpfs2/scratch/elongman/Nucella_can_drilling_genomics/data/processed/fastq_to_GL
+
+#--------------------------------------------------------------------------------
+
+# Use angsd to index pruned site list
+
+echo "Index sites with angsd"
+
+angsd sites index $WORKING_FOLDER/sites_info/sites_all_maf_R2.pruned
