7600facce77f8546454b56a8194083ca37dd787b
Emily Longman <Emily.Longman@uvm.edu>
2024-11-01 11:24:03 -0400

    Update 2_Multiqc.sh

--- src/Thermofly/1_QC/2_Multiqc.sh
+++ src/Thermofly/1_QC/2_Multiqc.sh
@@ -11,8 +11,6 @@
 
 #--------------------------------------------------------------------------------
 
-echo ${SLURM_ARRAY_TASK_ID}
-
 # Set folders and file locations
 working_folder=/gpfs2/scratch/elongman/Nucella_can_drilling_genomics/data/processed/Thermofly
 meta=$working_folder/METADATA/Thermofly_metadata.tsv
@@ -23,17 +21,8 @@ meta=$working_folder/METADATA/Thermofly_metadata.tsv
 module load python3.11-anaconda/2023.09-0
 source ${ANACONDA_ROOT}/etc/profile.d/conda.sh
 #conda create --name multiqc #If you haven't already done so, create and name the environment
-source activate multiqc #activate the environment
-#conda install -c bioconda multiqc # f you haven't already done so, install the program
-conda activate multiqc 
-
-#--------------------------------------------------------------------------------
-
-# Use metadata file to extract sample names and forward and reverse reads
-FIL1=$(cat ${meta} | awk -F '\t' '{print $24}' |  sed '1d' | sed "${SLURM_ARRAY_TASK_ID}q;d")
-FIL2=$(cat ${meta} | awk -F '\t' '{print $25}' |  sed '1d' | sed "${SLURM_ARRAY_TASK_ID}q;d")
-
-SAMP_NAME=$(cat ${meta} | awk -F '\t' '{print $1}' |  sed '1d' | sed "${SLURM_ARRAY_TASK_ID}q;d")
+conda activate multiqc #activate the environment
+#conda install -c bioconda multiqc # If you haven't already done so, install the program
 
 #--------------------------------------------------------------------------------
 
