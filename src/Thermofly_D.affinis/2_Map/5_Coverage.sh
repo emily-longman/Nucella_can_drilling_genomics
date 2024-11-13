12df6d250de78139e2f8a707ebfe3fe63e577383
Emily Longman <Emily.Longman@uvm.edu>
2024-11-13 11:24:36 -0500

    Update 5_Coverage.sh

--- src/Thermofly_D.affinis/2_Map/5_Coverage.sh
+++ src/Thermofly_D.affinis/2_Map/5_Coverage.sh
@@ -21,8 +21,7 @@ qualimap=/netfiles/nunezlab/Shared_Resources/Software/qualimap_v2.2.1/qualimap
 
 # Set folders and file locations
 working_folder=/gpfs2/scratch/elongman/Nucella_can_drilling_genomics/data/processed/Thermofly_D_affinis/
-#guide_file=$working_folder/METADATA/Qualimap_bam_list.txt
-guide_file=$working_folder/METADATA/Qualimap_bam_list_reduced.txt
+guide_file=$working_folder/METADATA/Qualimap_bam_list.txt
 
 #--------------------------------------------------------------------------------
 
@@ -33,12 +32,12 @@ JAVAMEM=18G # Java memory
 
 # Create output folders
 cd $working_folder
-mkdir bams_qualimap_multi_reduced
+mkdir bams_qualimap_multi
 
 #--------------------------------------------------------------------------------
 
 # Assess quality of bam files
 $qualimap multi-bamqc \
 -d $guide_file \
--outdir $working_folder/bams_qualimap_multi_reduced \
+-outdir $working_folder/bams_qualimap_multi \
 --java-mem-size=$JAVAMEM
