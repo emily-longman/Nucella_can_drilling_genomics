6d1cff536fde8ed9cb9b6c4dd22b63020f8fed67
Emily Longman <Emily.Longman@uvm.edu>
2024-11-18 11:50:52 -0500

    Update gwas

--- src/01_Fastq_to_GL/09_GWAS/01_gwas_binary.sh
+++ src/01_Fastq_to_GL/09_GWAS/01_gwas_binary.sh
@@ -103,7 +103,6 @@ angsd \
 -doMaf 1 -doMajorMinor 1 -doCounts 1 -doPost 1 \
 -remove_bads 1 -minMapQ 30 -minQ 20 \
 -minInd $MIN_IND -setMinDepthInd $MIN_DEPTH -minMaf $MIN_MAF -setMaxDepth $MAX_DEPTH \
--Pvalue 1 \
 -rf $WORKING_FOLDER/sites_info/regions_all_maf_pruned \
 -out $WORKING_FOLDER/GWAS/Nucella_SNPs_maf"$MIN_MAF"_pctind"$PERCENT_IND"_mindepth"$MIN_DEPTH"_maxdepth"$MAX_DEPTH_FACTOR".binary.gwas
 
