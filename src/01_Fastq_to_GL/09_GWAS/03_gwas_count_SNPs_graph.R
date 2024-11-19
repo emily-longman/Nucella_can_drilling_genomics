f3c7e0bcb6c194fc5991b1e2373022ca02f7f0bf
Emily Longman <Emily.Longman@uvm.edu>
2024-11-19 10:49:31 -0500

    Create Gwas mincount

--- src/01_Fastq_to_GL/09_GWAS/03_gwas_count_SNPs_graph.R
+++ src/01_Fastq_to_GL/09_GWAS/03_gwas_count_SNPs_graph.R
@@ -34,31 +34,31 @@ library(qqman) #https://cran.r-project.org/web/packages/qqman/vignettes/qqman.ht
 # ================================================================================== #
 
 # Load data 
-data.count <- read.table("Nucella_SNPs_maf0.05_pctind0.5_mindepth0.3_maxdepth2.count.SNPs.gwas.lrt0", header = T, sep = "\t")
-str(data.count)
+data.count.SNP <- read.table("Nucella_SNPs_maf0.05_pctind0.5_mindepth0.3_maxdepth2.count.SNPs.gwas.lrt0", header = T, sep = "\t")
+str(data.count.SNP)
 
 # Create unique Chromosome number 
-Chr.unique <- unique(data.count$Chromosome)
-data.count$CHR <- as.numeric(factor(data.count$Chromosome, levels = Chr.unique))
+Chr.unique <- unique(data.count.SNP$Chromosome)
+data.count.SNP$CHR <- as.numeric(factor(data.count.SNP$Chromosome, levels = Chr.unique))
 
 # Clean data
 # Remove LRT values that are -999 (i.e., Sites that fails one of the filters) and are negative
-data.count.filt <- data.count[-c(which(data.count$LRT == -999), which(data.count$LRT <= 0)), ]
+data.count.SNP.filt <- data.count.SNP[-c(which(data.count.SNP$LRT == -999), which(data.count.SNP$LRT <= 0)), ]
 
-hist(data.count.filt$LRT, breaks = 50)
+hist(data.count.SNP.filt$LRT, breaks = 50)
 
 # ================================================================================== #
 
 # Prepare data
 
 # Name each SNP 
-data.count.filt$SNP <- paste("r", 1:length(data.count.filt$Chromosome), sep="")
+data.count.SNP.filt$SNP <- paste("r", 1:length(data.count.SNP.filt$Chromosome), sep="")
 
 # Name each BP 
-data.count.filt$BP <- data.count.filt$Position
+data.count.SNP.filt$BP <- data.count.SNP.filt$Position
 
 # Get pvalues
-data.count.filt$P <- pchisq(data.count.filt$LRT, df=1, lower=F)
+data.count.SNP.filt$P <- pchisq(data.count.SNP.filt$LRT, df=1, lower=F)
 
 # Filter data (none of these in my data)
 #data.binary.filt <- data.binary.filt[-c(which(data.binary.filt$pvalue == "NaN" ),
@@ -68,24 +68,13 @@ data.count.filt$P <- pchisq(data.count.filt$LRT, df=1, lower=F)
 # ================================================================================== #
 
 # Make manhattan plot
-manhattan(data.count.filt, chr="CHR", bp="Position", p="P", 
+manhattan(data.count.SNP.filt, chr="CHR", bp="Position", p="P", 
           ylim = c(0, 350), cex = 0.6, suggestiveline = F, genomewideline = F, xlab = "Position")
 
 # Look at qq-plot of pvalues to check model fit
-qqnorm(data.count.filt$P)
+qqnorm(data.count.SNP.filt$P)
 
 # ================================================================================== #
-
-# Make manhattan plot prettier (https://github.com/pcgoddard/Burchardlab_Tutorials/wiki/GGplot2-Manhattan-Plot-Function)
-# Not working - maybe only ok for chromosomes 
-
-mypalette <- c("#5D82BB", "#3B64A5", "#1E4F9E", "#103B7E", "#082B64")
-hlight <- data.binary.filt$SNP[which(data.binary.filt$P < 0.00003)]
-sig = 5e-5 # significant threshold line
-sugg = 1e-6 
-
-#gg.manhattan(data.binary.filt, threshold=1e-6, hlight=hlight, col=NA, ylims=c(0,10), title="My Manhattan Plot")
-
 
 
 
