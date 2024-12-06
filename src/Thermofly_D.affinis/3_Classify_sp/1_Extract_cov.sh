bf6778215ba0ee55070238dda1d976b6ad199f4c
Emily Longman <Emily.Longman@uvm.edu>
2024-12-06 11:34:02 -0500

    Update 1_Extract_cov.sh

--- src/Thermofly_D.affinis/3_Classify_sp/1_Extract_cov.sh
+++ src/Thermofly_D.affinis/3_Classify_sp/1_Extract_cov.sh
@@ -4,6 +4,19 @@
 
 129-3862
 
-cat genome_results.txt | sed -n '129,3862p;3862q' | sed 's/^\t//g' > test.1
+# Extract coverage data for each contig
+cat genome_results.txt | sed -n '129,3862p;3862q' | sed 's/^\t//g' > test1.txt
 
-awk -i 'BEGIN{OFS="\t} {print $0} ()
+# Add column with sample name
+
+
+######
+
+#JCBN
+
+testfile=/netfiles/thermofly/ANALYSES/Affinis_Altitude/bamQCs/bams_qualimap/Qualimap_D_aff.wild.US-VT-Cam.1_7_2022.w.CH800D.1/genome_results.txt
+
+cat $testfile | sed -n '129,3862p;3862q' | sed -e 's/^\t//g' > test1.txt
+
+# Added column for each row that says "test1.txt"
+awk 'BEGIN{OFS="\t"} {print $0, (FNR>1 ? FILENAME : FILENAME)}' test1.txt | head
