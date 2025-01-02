acf381c365f6a8a21afecb8cdbe4e14234c87c48
Emily Longman <Emily.Longman@uvm.edu>
2025-01-02 15:58:00 -0500

    Update gwas scripts

--- src/01_Fastq_to_GL/05_Population_differentiation_drilling/06_graph_Fst.R
+++ src/01_Fastq_to_GL/05_Population_differentiation_drilling/06_graph_Fst.R
@@ -54,8 +54,14 @@ ggplot(data, aes(y=Fst, x=CHR.unique)) +
 # Rank-normalize Fst values
 
 data.rn <- data
-data.rn$rn_fst <- rank(data$Fst)/length(data$Fst)
-data.rn$rn_fst_r <- rank(data$Fst, ties="random")/length(data$Fst)
+data.rn$rank <- rank(-data$Fst)
+Lfst <- length(data$Fst)
+data.rn$rn_fst_r <- data.rn$rank/Lfst
+
+ggplot(data.rn, aes(y=-log10(rn_fst_r), x=CHR.unique)) + 
+  geom_point(col="black", alpha=0.8, size=1.3) + 
+  ylab("Window Fst") + xlab("Position") +
+  theme_bw()
 
 # Window analysis
 
@@ -63,3 +69,76 @@ win.bp <- 25000
 step.bp <- 5000
 
 
+wins <- foreach(chr.i=unique(data.rn$chr),
+                .combine="rbind", 
+                .errorhandling="remove")%do%{
+                  
+                  message(chr.i)
+                  
+                  tmp <- data.rn %>%
+                    filter(chr == chr.i)
+                  
+                  S=dim(tmp)[1]
+                
+                  if(S >= 30){
+                    o =
+                    data.table(chr=chr.i,
+                               S=dim(tmp)[1],
+                               start=seq(from=min(tmp$midPos), to=max(tmp$midPos)-win.bp, by=step.bp),
+                               end=seq(from=min(tmp$midPos), to=max(tmp$midPos)-win.bp, by=step.bp) + win.bp)
+                    return(o)
+                    
+                  }   
+                  else {message("fails S filter")}
+                }
+
+wins[,i:=1:dim(wins)[1]]
+####
+dim(wins)
+
+
+####
+
+
+### start the summarization process
+win.out <- foreach(win.i=1:dim(wins)[1], 
+#win.out <- foreach(win.i=1:10000, 
+                   .errorhandling = "remove",
+                   .combine = "rbind"
+)%do%{
+  
+  message(paste(win.i, dim(wins)[1], sep=" / "))
+  
+  
+  win.tmp <- data.rn %>%
+    filter(chr == wins[win.i]$chr) %>%
+    filter(midPos >= wins[win.i]$start & midPos <= wins[win.i]$end)
+  
+  pr.i <- c(0.05)
+  
+  win.tmp %>% 
+    filter(!is.na(rn_fst_r)) %>%
+    summarise(chr = wins[win.i]$chr,
+              pos_mean = mean(midPos),
+              pos_mean = mean(midPos),
+              pos_min = min(midPos),
+              pos_max = max(midPos),
+              win=win.i,
+              pr=pr.i,
+              rnp.pr=c(mean(rn_fst_r<=pr.i)),
+              rnp.binom.p=c(binom.test(sum(rn_fst_r<=pr.i), 
+                                       length(rn_fst_r), pr.i)$p.value),
+              max.fst=max(Fst),
+              nSNPs = n(),
+              sum.rnp=sum(rn_fst_r<=pr.i),
+    )  -> win.out
+  }
+
+
+
+win.out %>%
+  ggplot(aes(
+    x=max.fst,
+    y=-log10(rnp.binom.p)
+  )) + geom_point() + 
+  geom_hline(yintercept = -log10(0.05))
