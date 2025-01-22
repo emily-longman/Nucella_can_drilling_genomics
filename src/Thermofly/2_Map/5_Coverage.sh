18c67210162212d243bd06756465152bcbb77ea9
Emily Longman <Emily.Longman@uvm.edu>
2025-01-22 11:00:13 -0500

    Update 5_Coverage.sh

--- src/Thermofly/2_Map/5_Coverage.sh
+++ src/Thermofly/2_Map/5_Coverage.sh
@@ -3,10 +3,10 @@
 #SBATCH -J Coverage_bams  
 #SBATCH -c 1 
 #SBATCH -N 1 # on one node  
-#SBATCH -t 8:00:00   
-#SBATCH --mem 40G   
+#SBATCH -t 1:00:00   
+#SBATCH --mem 5G   
 #SBATCH --output=./slurmOutput/%x_%j.out  
-#SBATCH -p bluemoon  
+#SBATCH -p general  
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=emily.longman@uvm.edu 
 
@@ -21,8 +21,8 @@ qualimap=/netfiles/nunezlab/Shared_Resources/Software/qualimap_v2.2.1/qualimap
 
 # Set folders and file locations
 working_folder=/gpfs2/scratch/elongman/Nucella_can_drilling_genomics/data/processed/Thermofly
-#guide_file=$working_folder/METADATA/Qualimap_bam_list.txt
-guide_file=$working_folder/METADATA/Qualimap_bam_list_reduced.txt
+guide_file=$working_folder/METADATA/Qualimap_bam_list.txt
+guide_file_reduced=$working_folder/METADATA/Qualimap_bam_list_reduced.txt
 
 #--------------------------------------------------------------------------------
 
@@ -33,7 +33,7 @@ JAVAMEM=18G # Java memory
 
 # Create output folders
 cd $working_folder
-#mkdir bams_qualimap_multi
+mkdir bams_qualimap_multi
 mkdir bams_qualimap_multi_reduced
 
 #--------------------------------------------------------------------------------
@@ -41,5 +41,11 @@ mkdir bams_qualimap_multi_reduced
 # Assess quality of bam files
 $qualimap multi-bamqc \
 -d $guide_file \
+-outdir $working_folder/bams_qualimap_multi \
+--java-mem-size=$JAVAMEM
+
+# Assess quality of bam files
+$qualimap multi-bamqc \
+-d $guide_file_reduced \
 -outdir $working_folder/bams_qualimap_multi_reduced \
 --java-mem-size=$JAVAMEM
