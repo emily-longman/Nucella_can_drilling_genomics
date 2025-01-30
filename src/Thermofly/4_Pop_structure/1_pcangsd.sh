ba450b47cc74c8225cb17de5ded3f33678792aa4
Emily Longman <Emily.Longman@uvm.edu>
2025-01-30 18:30:32 -0500

    Update 1_pcangsd.sh

--- src/Thermofly/4_Pop_structure/1_pcangsd.sh
+++ src/Thermofly/4_Pop_structure/1_pcangsd.sh
@@ -6,7 +6,7 @@
 #SBATCH -t 8:00:00   
 #SBATCH --mem 40G   
 #SBATCH --output=./slurmOutput/%x_%j.out 
-#SBATCH -p bluemoon  
+#SBATCH -p general  
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=emily.longman@uvm.edu 
 
@@ -15,8 +15,8 @@
 # Generate pca
 
 # Load software  
-module load python3.11-anaconda
-module load gcc/10.5.0
+module load python3.11-anaconda/2024.02-1
+module load gcc/13.3.0-xp3epyt
 # pcangsd 
 export project_dir=/gpfs1/home/e/l/elongman/venv
 export venv_name=pcangsd-venv
@@ -27,7 +27,7 @@ source $venv_name/bin/activate
 
 # Set folders and file locations
 working_folder=/gpfs2/scratch/elongman/Nucella_can_drilling_genomics/data/processed/Thermofly
-ref=/netfiles/thermofly/GENOMES/basisetae/D.basisetae_nanopore.fasta.masked.fa
+ref=/netfiles/thermofly/GENOMES/basisetae/GCA_035041595.1_ASM3504159v1_genomic.fna.masked.fa
 script_folder=/gpfs2/scratch/elongman/Nucella_can_drilling_genomics/src/Thermofly
 bam_list=$working_folder/info/bam_filelist_reduced.list
 
