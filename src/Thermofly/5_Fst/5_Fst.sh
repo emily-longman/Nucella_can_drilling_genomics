d32e0dda0aa17c0165471cf00aafcdc07cc8974b
Emily Longman <Emily.Longman@uvm.edu>
2025-02-03 09:34:28 -0500

    Update 5_Fst.sh

--- src/Thermofly/5_Fst/5_Fst.sh
+++ src/Thermofly/5_Fst/5_Fst.sh
@@ -3,10 +3,10 @@
 #SBATCH -J Fst_calc  
 #SBATCH -c 6  
 #SBATCH -N 1 # on one node  
-#SBATCH -t 8:00:00   
+#SBATCH -t 4:00:00   
 #SBATCH --mem 40G   
 #SBATCH --output=./slurmOutput/%x_%j.out 
-#SBATCH -p bluemoon 
+#SBATCH -p general 
 #SBATCH --array=0-99%10
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=emily.longman@uvm.edu 
@@ -16,7 +16,8 @@
 # Calculate Fst between groups
 
 # Load software  
-spack load angsd@0.933
+module load gcc/13.3.0-xp3epyt
+module load angsd/0.935-4asngpy
 
 #--------------------------------------------------------------------------------
 
