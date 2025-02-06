215d20ddb8dad45baa0aa4c5c7dc26536d6da03d
Emily Longman <Emily.Longman@uvm.edu>
2025-02-06 13:27:47 -0500

    Rearrange scripts thermofly

--- src/Thermofly/6_Annotation/3_annotation_SNPs.R
+++ src/Thermofly/6_Annotation/3_annotation_SNPs.R
@@ -1,15 +1,8 @@
-# This script will annotate SNPs of interest
+# This script will annotate SNPs of interest.
 
-# Create Annotation dir in working_directory to test things out
-working_folder=/gpfs2/scratch/elongman/Nucella_can_drilling_genomics/data/processed/Thermofly
-cd $working_folder
-mkdir annotation
-scp SnpEff/D.bas_SNPs.annotate.vcf annotation/
-cd annotation
+# Prior to running this code, make sure the SnpEff vcf file from the previous step and the SNPs of interest are all in a directory called "Annotation"
 
 #--------------------------------------------------------------------------------
-
-module load R/4.4.1  
 
 # Load packages
 install.packages(c('data.table', 'tidyverse'))
@@ -24,10 +17,6 @@ BiocManager::install(version = "3.20")
 BiocManager::install("SeqArray")
 library(SeqArray)
 
-
-#--------------------------------------------------------------------------------
-
-
 #--------------------------------------------------------------------------------
 
 # Convert VCF to GDS
@@ -41,10 +30,7 @@ seqVCF2GDS(vcf.fn, "D.bas_SNPs.annotate.gds")
 
 #--------------------------------------------------------------------------------
 
-# Load GDS file
-#gds.fn <- seqExampleFileName("gds")
 # Open the GDS file
-
 geno.file <- seqOpen("/gpfs2/scratch/elongman/Nucella_can_drilling_genomics/data/processed/Thermofly/annotation/D.bas_SNPs.annotate.gds")
 # Display the contents of the GDS file
 geno.file
@@ -115,21 +101,9 @@ geno.file
 
 #--------------------------------------------------------------------------------
 
-
-#--------------------------------------------------------------------------------
-
-# Extract annotation from GDS
-#ann_data <- seqGetData(geno.file, "annotation/info/ANN")
-
-
-
-#--------------------------------------------------------------------------------
-
-
 # Load SNPs of interest
 snps <- read.csv("SNPs_of_interst.unq.txt", sep="\t")
 str(snps)
-
 
 #--------------------------------------------------------------------------------
 
@@ -137,27 +111,26 @@ str(snps)
 snp.dt <- data.table(chr=seqGetData(geno.file, "chromosome"),
                         pos=seqGetData(geno.file, "position"),
                         nAlleles=seqGetData(geno.file, "$num_allele"),
-                        id=seqGetData(geno.file, "variant.id"))
+                        id=seqGetData(geno.file, "variant.id")) %>%
+                        mutate(SNP_id = paste(chr, pos, sep = "_"))
 
-
-# Create id that combines chromosome and position
-snp.dt$SNP_id <- paste0(snp.dt$chr, "_", snp.dt$pos)
 
 #--------------------------------------------------------------------------------
 
 # Extract annotation data for each SNP of interest
 
-test <- snps[1:5,]
-
 annotation = 
-foreach(i=1:dim(test)[1], .combine = "rbind")%do%{
+foreach(i=1:dim(snps)[1], 
+.combine = "rbind")%do%{
+
 seqResetFilter(geno.file)
 
-chr.tmp = test$chr[i]
-pos.tmp = test$pos[i]
-#myvar = test %>% filter(chr == chr & pos == pos)[i]
+tmp.i = snps[i,]$SNP_id
 
-seqSetFilter(geno.file, chromosome = chr.tmp, position = pos.tmp, intersect = T)
+pos.tmp = snp.dt %>% filter(SNP_id %in% tmp.i) %>% .$id
+
+seqSetFilter(geno.file, variant.id = pos.tmp)
+
 ann_data <- seqGetData(geno.file, "annotation/info/ANN")$data
 
 L = length(ann_data)
@@ -165,9 +138,9 @@ foreach(k=1:L, .combine = "rbind")%do%{
   tmp = ann_data[k] 
   tmp2= str_split(tmp, "\\|")
   
-
   data.frame(
-    chr.pos=i,
+    id=pos.tmp,
+    SNP_id = tmp.i,
     annotation.id=k,
     Allele = tmp2[[1]][1],
     Annotation = tmp2[[1]][2],
@@ -190,150 +163,12 @@ foreach(k=1:L, .combine = "rbind")%do%{
 }
 }
 
-
-ERRORS_WARNINGS_INFO = tmp2[[1]][16],
+#--------------------------------------------------------------------------------
 
 # Join annotation and SNP information
 D.basisetae_annotated_SNPs <- left_join(annotation, snp.dt, by = join_by(id))
 
-
+# Write output
 write.csv(D.basisetae_annotated_SNPs, "/gpfs2/scratch/elongman/Nucella_can_drilling_genomics/data/processed/Thermofly/annotation/D.basisetae_annotated_SNPs.txt")
-
 write.csv(D.basisetae_annotated_SNPs, "/gpfs2/scratch/elongman/Nucella_can_drilling_genomics/data/processed/Thermofly/annotation/D.basisetae_annotated_SNPs.csv")
 
-#--------------------------------------------------------------------------------
-
-
-
-
-
-
-# Example code:
-
-# SNPs of interest
-adapt_cands = snp.dt[sample(10),]
-
-ann_ex = 
-foreach(i=1:dim(adapt_cands)[1], .combine = "rbind")%do%{
-seqResetFilter(geno.file)
-
-myvar = adapt_cands$id[i]
-
-seqSetFilter(geno.file, variant.id= myvar )
-ann_data <- seqGetData(geno.file, "annotation/info/ANN")$data
-
-L = length(ann_data)
-foreach(k=1:L, .combine = "rbind")%do%{
-  tmp = ann_data[k] 
-  tmp2= str_split(tmp, "\\|")
-  
-
-  data.frame(
-    id=i,
-    annotation.id=k,
-    allele=tmp2[[1]][1],
-    feature = tmp2[[1]][2]
-
-  )
-
-}
-}
-
-left_join(ann_ex, snp.dt)
-
-
-
-
-
-#--------------------------------------------------------------------------------
-# Extract data from GDS
-variant_ids <- seqGetData(geno.file, "variant.id")
-positions <- seqGetData(geno.file, "position")
-chromosomes <- seqGetData(geno.file, "chromosome")
-
-# Convert extracted data to a data frame
-variant_info <- data.frame(variant_id = variant_ids, pos = positions, chr = chromosomes)
-
-
-
-# Separate the 'data' and 'length' components
-ann_entries <- ann_data$data
-ann_lengths <- ann_data$length
-
-# Initialize a list placeholder to store the extracted data for each variant
-ann_list <- vector("list", length = length(ann_lengths))
-
-# Set an index to keep track of position in `ann_entries`
-index <- 1
-
-# Loop through each variant using the length of each entry to slice ann_entries
-for (i in seq_along(ann_lengths)) {
-  message("Processing variant ID: ", i)
-  
-  # Number of entries for this variant
-  n_entries <- ann_lengths[i]
-  
-  # Extract the entries for this variant
-  if (n_entries > 0) {
-    variant_ann <- ann_entries[index:(index + n_entries - 1)]
-    ann_list[[i]] <- strsplit(variant_ann, "\\|")  # Split each annotation by '|'
-    index <- index + n_entries
-  } else {
-    ann_list[[i]] <- list(rep(NA, 15))  # If no annotation, assign NA
-  }
-}
-
-# Flatten and convert the list into a data frame
-ann_df <- do.call(rbind, lapply(ann_list, function(x) {
-  do.call(rbind, lapply(x, function(y) if (!is.na(y)) y else rep(NA, 15)))
-}))
-
-
-# Set column names for the ann_df data frame
-colnames(ann_df) <- c("Allele", "Annotation", "Annotation_Impact", "Gene_Name", 
-                      "Gene_ID", "Feature_Type", "Feature_ID", 
-                      "Transcript_BioType", "Rank", "HGVS.c", 
-                      "HGVS.p", "cDNA.pos", "CDS.pos", 
-                      "AA.pos", "Distance", "ERRORS_WARNINGS_INFO")
-
-# Convert ann_df to a data frame
-ann_df <- as.data.frame(ann_df, stringsAsFactors = FALSE)
-
-# Ensure variant_info and ann_df have the same number of rows before combining
-if (nrow(variant_info) == nrow(ann_df)) {
-  # Combine variant information with annotation data
-  final_df <- cbind(variant_info, ann_df)
-} else {
-  stop("Number of variants in variant_info does not match number of rows in ann_df")
-}
-
-# Close the GDS file
-seqClose(genofile)
-
-#--------------------------------------------------------------------------------
-
-# Check the combined data
-head(final_df)
-
-###load SNP list interested in
-significant_windows<-load("windowed.RData")
-
-
-###mine looks like this:
-#test_code            chr           variable pos_mean pos_min pos_max win
-#1         0 NW_022145594.1 ALLSKY_SFC_UVB_min    54798    7823  102218   1
-#2         1 NW_022145594.1 ALLSKY_SFC_UVB_min    54798    7823  102218   1
-#3         2 NW_022145594.1 ALLSKY_SFC_UVB_min    54798    7823  102218   1
-#rnp.pr.0.05 rnp.pr.0.01 rnp.pr.0.001 rnp.binom.p.0.05 rnp.binom.p.0.01
-#1  0.01620370 0.001157407  0.000000000     2.881103e-07      0.003111481
-#2  0.03587963 0.009259259  0.001157407     6.045702e-02      1.000000000
-#3  0.04166667 0.005787037  0.000000000     3.093977e-01      0.300537578
-
-###used fuzzy join to join my windowed analysis (simpler if its 1:1 if looking at individual SNPS)
-filtered_snps <- final_df %>%
-  fuzzy_inner_join(
-    significant_windows,
-    by = c("chr" = "chr", "variable" = "variable", "pos" = "pos_min", "pos" = "pos_max"),
-    match_fun = list(`==`, `==`, `>=`, `<=`)
-  ) %>%
-  distinct()
