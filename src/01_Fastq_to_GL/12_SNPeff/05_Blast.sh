b373dd915c153b79913b975db07fbb58b6e07879
Emily Longman <Emily.Longman@uvm.edu>
2025-03-09 20:28:22 -0400

    Update 05_Blast.sh

--- src/01_Fastq_to_GL/12_SNPeff/05_Blast.sh
+++ src/01_Fastq_to_GL/12_SNPeff/05_Blast.sh
@@ -20,7 +20,7 @@
 #SBATCH --mem=5G
 
 # Submit job array
-#SBATCH --array=1-999%40
+#SBATCH --array=1-800%30
 
 # Name output of this job using %x=job-name and %j=job-id
 #SBATCH --output=./slurmOutput/%x.%A_%a.out  # Standard output
